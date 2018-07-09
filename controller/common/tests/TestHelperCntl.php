d185a24eb3da2c6e71a447928dd1c26b81cdfe17
Aimeos <aimeos@aimeos.org>
2018-07-09 13:14:24 +0200

    Minor

--- controller/common/tests/TestHelperCntl.php
+++ controller/common/tests/TestHelperCntl.php
@@ -53,7 +53,7 @@ class TestHelperCntl
 		$aimeos = self::getAimeos();
 
 
-		$paths = $aimeos->getConfigPaths( 'mysql' );
+		$paths = $aimeos->getConfigPaths();
 		$paths[] = __DIR__ . DIRECTORY_SEPARATOR . 'config';
 		$file = __DIR__ . DIRECTORY_SEPARATOR . 'confdoc.ser';
 
