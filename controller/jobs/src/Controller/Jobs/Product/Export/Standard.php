6e9f1eafda1047a3313c478fcc54f02922747806
Marian Deimel <40670659+creativemadeofficial@users.noreply.github.com>
2018-08-16 15:11:45 +0200

    Update Standard.php

--- controller/jobs/src/Controller/Jobs/Product/Export/Standard.php
+++ controller/jobs/src/Controller/Jobs/Product/Export/Standard.php
@@ -288,19 +288,19 @@ class Standard
 		$start = 0; $filenum = 1;
 		$names = [];
 
-        $indexManager = \Aimeos\MShop\Index\Manager\Factory::createManager($this->getContext());
+        	$indexManager = \Aimeos\MShop\Index\Manager\Factory::createManager($this->getContext());
 
-        $search = $indexManager->createSearch($default);
-        $search->setSlice(0, $maxQuery);
-        $search->setConditions(
-            $search->compare('!=', 'index.catalog.id', null)
-        );
-        $search->setSortations(
-            [$search->sort('+', 'product.id')]
-        );
+       		$search = $indexManager->createSearch($default);
+        	$search->setSlice(0, $maxQuery);
+        	$search->setConditions(
+           		$search->compare('!=', 'index.catalog.id', null)
+        	);
+        	$search->setSortations(
+                	[$search->sort('+', 'product.id')]
+        	);
 
-        $content = $this->createContent($container, $filenum);
-        $names[] = basename($content->getResource());
+        	$content = $this->createContent($container, $filenum);
+        	$names[] = basename($content->getResource());
 
 		do
 		{
