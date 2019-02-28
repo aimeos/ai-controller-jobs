e68c4298cc747799c2b5d93abddf11e5af4486d1
Aimeos <aimeos@aimeos.org>
2019-02-28 11:44:39 +0100

    Implemented customer XML importer

---
+++ controller/jobs/tests/Controller/Jobs/Customer/Import/Xml/StandardTest.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015-2018
+ */
+
+
+namespace Aimeos\Controller\Jobs\Customer\Import\Xml;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $object;
+	private $context;
+	private $aimeos;
+
+
+	protected function setUp()
+	{
+		\Aimeos\MShop::cache( true );
+
+		$this->context = \TestHelperJobs::getContext();
+		$this->aimeos = \TestHelperJobs::getAimeos();
+
+		$config = $this->context->getConfig();
+		$config->set( 'controller/jobs/customer/import/xml/location', __DIR__ . '/_testfiles' );
+
+		$this->object = new \Aimeos\Controller\Jobs\Customer\Import\Xml\Standard( $this->context, $this->aimeos );
+	}
+
+
+	protected function tearDown()
+	{
+		\Aimeos\MShop::cache( false );
+		unset( $this->object, $this->context, $this->aimeos );
+	}
+
+
+	public function testGetName()
+	{
+		$this->assertEquals( 'Customer import XML', $this->object->getName() );
+	}
+
+
+	public function testGetDescription()
+	{
+		$text = 'Imports new and updates existing customers from XML files';
+		$this->assertEquals( $text, $this->object->getDescription() );
+	}
+
+
+	public function testRun()
+	{
+		$this->object->run();
+
+		$manager = \Aimeos\MShop::create( $this->context, 'customer' );
+		$item = $manager->findItem( 'me@example.com', ['customer/address', 'customer/property', 'media', 'text'] );
+		$manager->deleteItem( $item->getId() );
+
+		$this->assertEquals( 'Test user', $item->getLabel() );
+		$this->assertEquals( 1, count( $item->getPropertyItems() ) );
+		$this->assertEquals( 1, count( $item->getRefItems( 'text' ) ) );
+		$this->assertEquals( 1, count( $item->getRefItems( 'media' ) ) );
+		$this->assertEquals( 'mrs', $item->getPaymentAddress()->getSalutation() );
+		$this->assertEquals( 'Example street', $item->getPaymentAddress()->getAddress1() );
+		$this->assertEquals( 'mrs', current( $item->getAddressItems() )->getSalutation() );
+	}
+}
