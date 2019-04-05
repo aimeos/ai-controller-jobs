65bd25c3873824874947512f8e48a7ddcc9e9157
Aimeos <aimeos@aimeos.org>
2019-04-05 12:34:31 +0200

    Fixed documentation

--- controller/jobs/src/Controller/Jobs/Subscription/Process/Renew/Standard.php
+++ controller/jobs/src/Controller/Jobs/Subscription/Process/Renew/Standard.php
@@ -135,6 +135,7 @@ class Standard
 	protected function addBasketCoupons( \Aimeos\MShop\Context\Item\Iface $context, \Aimeos\MShop\Order\Item\Base\Iface $basket, array $codes )
 	{
 		/** controller/jobs/subcription/process/renew/standard/use-coupons
+		 * Applies the coupons of the previous order also to the new one
 		 *
 		 * Reuse coupon codes added to the basket by the customer the first time
 		 * again in new subcription orders. If they have any effect depends on
