11be319586e321afa6ca4d02cf02cb8895870e25
Aimeos <aimeos@aimeos.org>
2019-04-12 19:36:50 +0200

    Implemented group import from customer XMLs

---
+++ controller/common/src/Controller/Common/Customer/Import/Xml/Processor/Group/Standard.php
@@ -0,0 +1,97 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2019
+ * @package Controller
+ * @subpackage Common
+ */
+
+
+namespace Aimeos\Controller\Common\Customer\Import\Xml\Processor\Group;
+
+
+/**
+ * Group processor for customer XML imports
+ *
+ * @package Controller
+ * @subpackage Common
+ */
+class Standard
+	extends \Aimeos\Controller\Common\Common\Import\Xml\Processor\Base
+	implements \Aimeos\Controller\Common\Common\Import\Xml\Processor\Iface
+{
+	/** controller/common/customer/import/xml/processor/group/name
+	 * Name of the group processor implementation
+	 *
+	 * Use "Myname" if your class is named "\Aimeos\Controller\Common\Customer\Import\Xml\Processor\Group\Myname".
+	 * The name is case-sensitive and you should avoid camel case names like "MyName".
+	 *
+	 * @param string Last part of the processor class name
+	 * @since 2019.04
+	 * @category Developer
+	 */
+
+	 private $groupManager;
+	 private $groups = [];
+
+
+	/**
+	 * Initializes the object
+	 *
+	 * @param \Aimeos\MShop\Context\Item\Iface $context Context object
+	 */
+	public function __construct( \Aimeos\MShop\Context\Item\Iface $context )
+	{
+		parent::__construct( $context );
+
+		$this->groupManager = \Aimeos\MShop::create( $context, 'customer/group' );
+		$search = $this->groupManager->createSearch();
+		$start = 0;
+
+		do
+		{
+			$items = $this->groupManager->searchItems( $search->setSlice( $start, 100 ) );
+
+			foreach( $items as $item ) {
+				$this->groups[$item->getCode()] = $item->getId();
+			}
+
+			$count = count( $items );
+			$start += $count;
+			unset( $items );
+		}
+		while( $count === $search->getSliceSize() );
+	}
+
+
+	/**
+	 * Updates the given item using the data from the DOM node
+	 *
+	 * @param \Aimeos\MShop\Common\Item\Iface $item Item which should be updated
+	 * @param \DOMNode $node XML document node containing a list of nodes to process
+	 * @return \Aimeos\MShop\Common\Item\Iface Updated item
+	 */
+	public function process( \Aimeos\MShop\Common\Item\Iface $item, \DOMNode $node )
+	{
+		\Aimeos\MW\Common\Base::checkClass( \Aimeos\MShop\Customer\Item\Iface::class, $item );
+
+		$ids = [];
+
+		foreach( $node->childNodes as $node )
+		{
+			if( $node->nodeName === 'groupitem' && ( $refattr = $node->attributes->getNamedItem( 'ref' ) ) !== null )
+			{
+				$code = $refattr->nodeValue;
+
+				if( !isset( $this->map[$code] ) ) {
+					$this->map[$code] = $this->groupManager->findItem( $code )->getId();
+				}
+
+				$ids[] = $this->map[$code];
+			}
+		}
+
+		return $item->setGroups( $ids );
+	}
+}
