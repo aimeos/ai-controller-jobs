14cd336d1389584032da1b0e17de9a01f5300093
Aimeos <aimeos@aimeos.org>
2019-05-21 15:02:25 +0200

    Fixed importing multiple prices with same values

--- controller/common/src/Controller/Common/Product/Import/Csv/Processor/Price/Standard.php
+++ controller/common/src/Controller/Common/Product/Import/Csv/Processor/Price/Standard.php
@@ -118,7 +118,7 @@ class Standard
 		foreach( $listItems as $listItem )
 		{
 			if( ( $refItem = $listItem->getRefItem() ) !== null ) {
-				$listMap[$refItem->getValue()][$refItem->getType()][$listItem->getType()] = $listItem;
+				$listMap[ $refItem->getType() ][ $listItem->getType() ][] = $listItem;
 			}
 		}
 
@@ -128,20 +128,17 @@ class Standard
 				continue;
 			}
 
-			$value = $this->getValue( $list, 'price.value', '0.00' );
 			$type = $this->getValue( $list, 'price.type', 'default' );
 			$listtype = $this->getValue( $list, 'product.lists.type', 'default' );
 
-			if( isset( $listMap[$value][$type][$listtype] ) )
-			{
-				$listItem = $listMap[$value][$type][$listtype];
-				$refItem = $listItem->getRefItem();
-				unset( $listItems[$listItem->getId()] );
+			if( isset( $listMap[$type][$listtype] ) && !empty( $listMap[$type][$listtype] ) ) {
+				$listItem = array_shift( $listMap[$type][$listtype] );
+			} else {
+				$listItem = $listManager->createItem();
 			}
-			else
-			{
-				$listItem = $listManager->createItem()->setType( $listtype );
-				$refItem = $manager->createItem()->setType( $type );
+
+			if( ( $refItem = $listItem->getRefItem() ) === null ) {
+				$refItem = $manager->createItem();
 			}
 
 			$listItem = $listItem->setPosition( $pos )->fromArray( $list );
@@ -150,6 +147,8 @@ class Standard
 			$refItem = $refItem->setLabel( $label )->fromArray( $list );
 
 			$product->addListItem( 'price', $listItem, $refItem );
+
+			unset( $listItems[$listItem->getId()] );
 		}
 
 		$product->deleteListItems( $listItems, true );
