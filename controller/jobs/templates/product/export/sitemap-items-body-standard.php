3e96aca09ca46d7e40975ba1a8b5afc93d6810e3
Aimeos <aimeos@aimeos.org>
2019-07-19 10:36:47 +0200

    Use absolute URLs for sitemap URLs

--- controller/jobs/templates/product/export/sitemap-items-body-standard.php
+++ controller/jobs/templates/product/export/sitemap-items-body-standard.php
@@ -10,7 +10,9 @@ $enc = $this->encoder();
 $detailTarget = $this->config( 'client/html/catalog/detail/url/target' );
 $detailCntl = $this->config( 'client/html/catalog/detail/url/controller', 'catalog' );
 $detailAction = $this->config( 'client/html/catalog/detail/url/action', 'detail' );
+$detailFilter = array_flip( $this->config( 'client/html/catalog/detail/url/filter', ['d_prodid'] ) );
 $detailConfig = $this->config( 'client/html/catalog/detail/url/config', [] );
+$detailConfig['absoluteUri'] = true;
 
 $freq = $enc->xml( $this->get( 'siteFreq', 'daily' ) );
 
@@ -18,7 +20,7 @@ $freq = $enc->xml( $this->get( 'siteFreq', 'daily' ) );
 <?php foreach( $this->get( 'siteItems', [] ) as $id => $item ) : ?>
 <?php
 		$date = str_replace( ' ', 'T', $item->getTimeModified() ) . date( 'P' );
-		$params = array( 'd_name' => $item->getName( 'url' ), 'd_prodid' => $id );
+		$params = array_diff_key( ['d_name' => $item->getName( 'url' ), 'd_prodid' => $id, 'd_pos' => ''], $detailFilter );
 		$url = $this->url( $detailTarget, $detailCntl, $detailAction, $params, [], $detailConfig );
 ?>
 	<url><loc><?php echo $enc->xml( $url ); ?></loc><lastmod><?php echo $date; ?></lastmod><changefreq><?php echo $freq; ?></changefreq></url>
