409078dba32300898af1e5cf0597ef9f40ec6c90
Aimeos <aimeos@aimeos.org>
2019-08-14 10:51:38 +0200

    Implemented parallel import of coupon code files

--- controller/jobs/src/Controller/Jobs/Coupon/Import/Csv/Code/Standard.php
+++ controller/jobs/src/Controller/Jobs/Coupon/Import/Csv/Code/Standard.php
@@ -50,71 +50,14 @@ class Standard
 	 */
 	public function run()
 	{
-		$total = $errors = 0;
-		$context = $this->getContext();
-		$config = $context->getConfig();
-		$logger = $context->getLogger();
-		$mappings = $this->getDefaultMapping();
-
-
-		/** controller/jobs/coupon/import/csv/code/mapping
-		 * List of mappings between the position in the CSV file and item keys
-		 *
-		 * This configuration setting overwrites the shared option
-		 * "controller/common/coupon/import/csv/mapping" if you need a
-		 * specific setting for the job controller. Otherwise, you should
-		 * use the shared option for consistency.
-		 *
-		 * @param array Associative list of processor names and lists of key/position pairs
-		 * @since 2017.10
-		 * @category Developer
-		 * @see controller/jobs/coupon/import/csv/code/skip-lines
-		 * @see controller/jobs/coupon/import/csv/code/max-size
-		 */
-		$mappings = $config->get( 'controller/jobs/coupon/import/csv/code/mapping', $mappings );
-
-
-		/** controller/jobs/coupon/import/csv/code/max-size
-		 * Maximum number of CSV rows to import at once
-		 *
-		 * It's more efficient to read and import more than one row at a time
-		 * to speed up the import. Usually, the bigger the chunk that is imported
-		 * at once, the less time the importer will need. The downside is that
-		 * the amount of memory required by the import process will increase as
-		 * well. Therefore, it's a trade-off between memory consumption and
-		 * import speed.
-		 *
-		 * @param integer Number of rows
-		 * @since 2017.10
-		 * @category Developer
-		 * @see controller/jobs/coupon/import/csv/code/skip-lines
-		 * @see controller/jobs/coupon/import/csv/code/mapping
-		 */
-		$maxcnt = (int) $config->get( 'controller/jobs/coupon/import/csv/code/max-size', 1000 );
-
-
-		/** controller/jobs/coupon/import/csv/code/skip-lines
-		 * Number of rows skipped in front of each CSV files
-		 *
-		 * Some CSV files contain header information describing the content of
-		 * the column values. These data is for informational purpose only and
-		 * can't be imported into the database. Using this option, you can
-		 * define the number of lines that should be left out before the import
-		 * begins.
-		 *
-		 * @param integer Number of rows
-		 * @since 2015.08
-		 * @category Developer
-		 * @see controller/jobs/coupon/import/csv/code/mapping
-		 * @see controller/jobs/coupon/import/csv/code/max-size
-		 */
-		$skiplines = (int) $config->get( 'controller/jobs/coupon/import/csv/code/skip-lines', 0 );
-
+		$fcn = function( \Aimeos\MW\Container\Iface $container, $couponId, $path ) {
+			$this->process( $container, $couponId, $path );
+		};
 
 		try
 		{
-			$processor = $this->getProcessors( $mappings );
-			$codePos = $this->getCodePosition( $mappings['code'] );
+			$context = $this->getContext();
+			$process = $context->getProcess();
 			$fs = $context->getFileSystemManager()->get( 'fs-import' );
 			$dir = 'couponcode/' . $context->getLocale()->getSite()->getCode();
 
@@ -128,44 +71,19 @@ class Standard
 					continue;
 				}
 
+				$path = $dir . '/' . $filename;
 				list( $couponId,) = explode( '.', $filename );
-				$container = $this->getContainer( $fs->readf( $dir . '/' . $filename ) );
+				$container = $this->getContainer( $fs->readf( $path ) );
 
-				$msg = sprintf( 'Started coupon import from "%1$s" (%2$s)', $filename, __CLASS__ );
-				$logger->log( $msg, \Aimeos\MW\Logger\Base::NOTICE );
-
-				foreach( $container as $content )
-				{
-					for( $i = 0; $i < $skiplines; $i++ ) {
-						$content->next();
-					}
-
-					while( ( $data = $this->getData( $content, $maxcnt, $codePos ) ) !== [] )
-					{
-						$items = $this->getCouponCodeItems( array_keys( $data ) );
-						$errcnt = $this->import( $items, $data, $couponId, $processor );
-						$chunkcnt = count( $data );
-
-						$msg = 'Imported coupon lines from "%1$s": %2$d/%3$d (%4$s)';
-						$logger->log( sprintf( $msg, $filename, $chunkcnt - $errcnt, $chunkcnt, __CLASS__ ), \Aimeos\MW\Logger\Base::NOTICE );
-
-						$errors += $errcnt;
-						$total += $chunkcnt;
-						unset( $items, $data );
-					}
-				}
-
-				$container->close();
-				$fs->rm( $dir . '/' . $filename );
-
-				$msg = 'Finished coupon import: %1$d successful, %2$s errors, %3$s total (%4$s)';
-				$logger->log( sprintf( $msg, $total - $errors, $errors, $total, __CLASS__ ), \Aimeos\MW\Logger\Base::NOTICE );
+				$process->start( $fcn, [$container, $couponId, $path] );
 			}
+
+			$process->wait();
 		}
 		catch( \Exception $e )
 		{
-			$logger->log( 'Coupon import error: ' . $e->getMessage() );
-			$logger->log( $e->getTraceAsString() );
+			$context->getLogger()->log( 'Coupon import error: ' . $e->getMessage() );
+			$context->getLogger()->log( $e->getTraceAsString() );
 
 			throw $e;
 		}
@@ -311,5 +229,106 @@ class Standard
 		}
 
 		return $errors;
+	}
+
+
+	/**
+	 * Imports content from the given container
+	 *
+	 * @param \Aimeos\MW\Container\Iface $container File container object
+	 * @param string $couponId Unique coupon ID the codes should be imported for
+	 * @param string $path Path to the container file
+	 */
+	protected function process( \Aimeos\MW\Container\Iface $container, $couponId, $path )
+	{
+		$total = $errors = 0;
+		$context = $this->getContext();
+		$config = $context->getConfig();
+		$logger = $context->getLogger();
+
+		/** controller/jobs/coupon/import/csv/code/mapping
+		 * List of mappings between the position in the CSV file and item keys
+		 *
+		 * This configuration setting overwrites the shared option
+		 * "controller/common/coupon/import/csv/mapping" if you need a
+		 * specific setting for the job controller. Otherwise, you should
+		 * use the shared option for consistency.
+		 *
+		 * @param array Associative list of processor names and lists of key/position pairs
+		 * @since 2017.10
+		 * @category Developer
+		 * @see controller/jobs/coupon/import/csv/code/skip-lines
+		 * @see controller/jobs/coupon/import/csv/code/max-size
+		 */
+		$mappings = $config->get( 'controller/jobs/coupon/import/csv/code/mapping', $this->getDefaultMapping() );
+
+		/** controller/jobs/coupon/import/csv/code/max-size
+		 * Maximum number of CSV rows to import at once
+		 *
+		 * It's more efficient to read and import more than one row at a time
+		 * to speed up the import. Usually, the bigger the chunk that is imported
+		 * at once, the less time the importer will need. The downside is that
+		 * the amount of memory required by the import process will increase as
+		 * well. Therefore, it's a trade-off between memory consumption and
+		 * import speed.
+		 *
+		 * @param integer Number of rows
+		 * @since 2017.10
+		 * @category Developer
+		 * @see controller/jobs/coupon/import/csv/code/skip-lines
+		 * @see controller/jobs/coupon/import/csv/code/mapping
+		 */
+		$maxcnt = (int) $config->get( 'controller/jobs/coupon/import/csv/code/max-size', 1000 );
+
+		/** controller/jobs/coupon/import/csv/code/skip-lines
+		 * Number of rows skipped in front of each CSV files
+		 *
+		 * Some CSV files contain header information describing the content of
+		 * the column values. These data is for informational purpose only and
+		 * can't be imported into the database. Using this option, you can
+		 * define the number of lines that should be left out before the import
+		 * begins.
+		 *
+		 * @param integer Number of rows
+		 * @since 2015.08
+		 * @category Developer
+		 * @see controller/jobs/coupon/import/csv/code/mapping
+		 * @see controller/jobs/coupon/import/csv/code/max-size
+		 */
+		$skiplines = (int) $config->get( 'controller/jobs/coupon/import/csv/code/skip-lines', 0 );
+
+
+		$msg = sprintf( 'Started coupon import from "%1$s" (%2$s)', $path, __CLASS__ );
+		$logger->log( $msg, \Aimeos\MW\Logger\Base::NOTICE );
+
+		$processor = $this->getProcessors( $mappings );
+		$codePos = $this->getCodePosition( $mappings['code'] );
+
+		foreach( $container as $content )
+		{
+			for( $i = 0; $i < $skiplines; $i++ ) {
+				$content->next();
+			}
+
+			while( ( $data = $this->getData( $content, $maxcnt, $codePos ) ) !== [] )
+			{
+				$items = $this->getCouponCodeItems( array_keys( $data ) );
+				$errcnt = $this->import( $items, $data, $couponId, $processor );
+				$chunkcnt = count( $data );
+
+				$msg = 'Imported coupon lines from "%1$s": %2$d/%3$d (%4$s)';
+				$logger->log( sprintf( $msg, $path, $chunkcnt - $errcnt, $chunkcnt, __CLASS__ ), \Aimeos\MW\Logger\Base::NOTICE );
+
+				$errors += $errcnt;
+				$total += $chunkcnt;
+				unset( $items, $data );
+			}
+		}
+
+		$msg = 'Finished coupon import: %1$d successful, %2$s errors, %3$s total (%4$s)';
+		$logger->log( sprintf( $msg, $total - $errors, $errors, $total, __CLASS__ ), \Aimeos\MW\Logger\Base::NOTICE );
+
+		$container->close();
+		$context->getFileSystemManager()->get( 'fs-import' )->rm( $path );
 	}
 }
