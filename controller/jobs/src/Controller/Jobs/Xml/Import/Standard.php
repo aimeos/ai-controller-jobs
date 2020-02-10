f7fca3b3984dd20f38dacb6956b4b33c255067ee
Aimeos <aimeos@aimeos.org>
2020-02-10 17:48:46 +0100

    Added job controller for executing all XML importers

---
+++ controller/jobs/src/Controller/Jobs/Xml/Import/Standard.php
@@ -0,0 +1,69 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2020
+ * @package Controller
+ * @subpackage Jobs
+ */
+
+
+namespace Aimeos\Controller\Jobs\Xml\Import;
+
+
+/**
+ * Job controller for XML imports
+ *
+ * @package Controller
+ * @subpackage Jobs
+ */
+class Standard
+	extends \Aimeos\Controller\Jobs\Base
+	implements \Aimeos\Controller\Jobs\Iface
+{
+	/**
+	 * Returns the localized name of the job.
+	 *
+	 * @return string Name of the job
+	 */
+	public function getName() : string
+	{
+		return $this->getContext()->getI18n()->dt( 'controller/jobs', 'All XML import' );
+	}
+
+
+	/**
+	 * Returns the localized description of the job.
+	 *
+	 * @return string Description of the job
+	 */
+	public function getDescription() : string
+	{
+		return $this->getContext()->getI18n()->dt( 'controller/jobs', 'Executes all XML importers and rebuild the index' );
+	}
+
+
+	/**
+	 * Executes the job.
+	 *
+	 * @throws \Aimeos\Controller\Jobs\Exception If an error occurs
+	 */
+	public function run()
+	{
+		$aimeos = $this->getAimeos();
+		$context = $this->getContext();
+		$logger = $context->getLogger();
+
+		$logger->log( 'Started XML import', \Aimeos\MW\Logger\Base::INFO );
+
+		\Aimeos\Controller\Jobs\Attribute\Import\Xml\Factory::create( $context, $aimeos )->run();
+		\Aimeos\Controller\Jobs\Customer\Import\Xml\Factory::create( $context, $aimeos )->run();
+		\Aimeos\Controller\Jobs\Product\Import\Xml\Factory::create( $context, $aimeos )->run();
+		\Aimeos\Controller\Jobs\Supplier\Import\Xml\Factory::create( $context, $aimeos )->run();
+		\Aimeos\Controller\Jobs\Catalog\Import\Xml\Factory::create( $context, $aimeos )->run();
+
+		\Aimeos\Controller\Jobs\Index\Rebuild\Factory::create( $context, $aimeos )->run();
+
+		$logger->log( 'Finished XML import', \Aimeos\MW\Logger\Base::INFO );
+	}
+}
