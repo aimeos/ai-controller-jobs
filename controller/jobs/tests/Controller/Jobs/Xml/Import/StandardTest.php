f69c23cde18831522339688e56a26b9d9eb6a442
Aimeos <aimeos@aimeos.org>
2020-02-10 17:48:46 +0100

    Added job controller for executing all XML importers

---
+++ controller/jobs/tests/Controller/Jobs/Xml/Import/StandardTest.php
@@ -0,0 +1,60 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015-2020
+ */
+
+
+namespace Aimeos\Controller\Jobs\Xml\Import;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+
+
+	protected function setUp()
+	{
+		\Aimeos\MShop::cache( true );
+
+		$aimeos = \TestHelperJobs::getAimeos();
+		$this->context = \TestHelperJobs::getContext();
+
+		$this->object = new \Aimeos\Controller\Jobs\Xml\Import\Standard( $this->context, $aimeos );
+	}
+
+
+	protected function tearDown()
+	{
+		unset( $this->object, $this->context, $this->aimeos );
+		\Aimeos\MShop::cache( false );
+	}
+
+
+	public function testGetName()
+	{
+		$this->assertEquals( 'All XML import', $this->object->getName() );
+	}
+
+
+	public function testGetDescription()
+	{
+		$text = 'Executes all XML importers and rebuild the index';
+		$this->assertEquals( $text, $this->object->getDescription() );
+	}
+
+
+	public function testRun()
+	{
+		$config = $this->context->getConfig();
+		$config->set( 'controller/jobs/attribute/import/xml/location', __DIR__ . '/_testfiles' );
+		$config->set( 'controller/jobs/customer/import/xml/location', __DIR__ . '/_testfiles' );
+		$config->set( 'controller/jobs/product/import/xml/location', __DIR__ . '/_testfiles' );
+		$config->set( 'controller/jobs/supplier/import/xml/location', __DIR__ . '/_testfiles' );
+		$config->set( 'controller/jobs/catalog/import/xml/location', __DIR__ . '/_testfiles' );
+
+		$this->object->run();
+	}
+}
