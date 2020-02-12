95c9620af669fb92ad8ed4be581e610259a44f99
Aimeos <aimeos@aimeos.org>
2020-02-12 10:17:16 +0100

    Ensure only orders with products from same site are exported

--- controller/jobs/src/Controller/Jobs/Order/Export/Csv/Standard.php
+++ controller/jobs/src/Controller/Jobs/Order/Export/Csv/Standard.php
@@ -263,7 +263,13 @@ class Standard
 
 		$container = $this->getContainer();
 		$content = $container->create( 'order-export_' . date( 'Y-m-d_H-i-s' ) );
+
 		$search = $this->initCriteria( $manager->createSearch()->setSlice( 0, 0x7fffffff ), $msg );
+		$search->setConditions( $search->combine( '&&', [
+			$search->compare( '!=', 'order.base.product.siteid', '' ),
+			$search->getConditions()
+		] ) );
+
 		$start = 0;
 
 		do
