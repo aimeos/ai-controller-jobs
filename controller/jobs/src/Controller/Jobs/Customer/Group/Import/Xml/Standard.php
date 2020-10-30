bb30bcf296dca92df5d762a035b619af2512aede
Aimeos <aimeos@aimeos.org>
2020-10-30 11:10:26 +0100

    Fixed method name

--- controller/jobs/src/Controller/Jobs/Customer/Group/Import/Xml/Standard.php
+++ controller/jobs/src/Controller/Jobs/Customer/Group/Import/Xml/Standard.php
@@ -254,7 +254,7 @@ class Standard
 		$search = $manager->createSearch()->setSlice( 0, count( $codes ) );
 		$search->setConditions( $search->compare( '==', 'customer.group.code', array_keys( $codes ) ) );
 
-		foreach( $manager->search( $search, $ref ) as $item ) {
+		foreach( $manager->searchItems( $search, $ref ) as $item ) {
 			$map[$item->getCode()] = $item;
 		}
 
