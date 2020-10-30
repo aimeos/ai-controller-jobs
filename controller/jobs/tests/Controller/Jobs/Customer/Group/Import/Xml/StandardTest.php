35cd17c1219a936d2a61835937e47bb0cdea8120
Aimeos <aimeos@aimeos.org>
2020-10-30 10:47:56 +0100

    Implemented XML import of customer groups

---
+++ controller/jobs/tests/Controller/Jobs/Customer/Group/Import/Xml/StandardTest.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2015-2020
+ */
+
+
+namespace Aimeos\Controller\Jobs\Customer\Group\Import\Xml;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $object;
+	private $context;
+	private $aimeos;
+
+
+	protected function setUp() : void
+	{
+		\Aimeos\MShop::cache( true );
+
+		$this->context = \TestHelperJobs::getContext();
+		$this->aimeos = \TestHelperJobs::getAimeos();
+
+		$config = $this->context->getConfig();
+		$config->set( 'controller/jobs/customer/group/import/xml/location', __DIR__ . '/_testfiles' );
+
+		$this->object = new \Aimeos\Controller\Jobs\Customer\Group\Import\Xml\Standard( $this->context, $this->aimeos );
+	}
+
+
+	protected function tearDown() : void
+	{
+		\Aimeos\MShop::cache( false );
+		unset( $this->object, $this->context, $this->aimeos );
+	}
+
+
+	public function testGetName()
+	{
+		$this->assertEquals( 'Customer group import XML', $this->object->getName() );
+	}
+
+
+	public function testGetDescription()
+	{
+		$text = 'Imports new and updates existing customer groups from XML files';
+		$this->assertEquals( $text, $this->object->getDescription() );
+	}
+
+
+	public function testRun()
+	{
+		$this->object->run();
+
+		$manager = \Aimeos\MShop::create( $this->context, 'customer/group' );
+		$item = $manager->findItem( 'test' );
+		$manager->deleteItem( $item );
+
+		$this->assertEquals( 'Test group', $item->getLabel() );
+		$this->assertEquals( 'test', $item->getCode() );
+	}
+}
