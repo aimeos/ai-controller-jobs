feb273f10d456fbd3ec0aa021877a44e83ba79b6
Aimeos <aimeos@aimeos.org>
2021-12-11 13:00:47 +0100

    Implemented importing suppliers in product XML import

---
+++ controller/common/src/Controller/Common/Common/Import/Xml/Processor/Lists/Supplier/Standard.php
@@ -0,0 +1,123 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2021
+ * @package Controller
+ * @subpackage Common
+ */
+
+
+namespace Aimeos\Controller\Common\Common\Import\Xml\Processor\Lists\Supplier;
+
+
+/**
+ * Supplier list processor for XML imports
+ *
+ * @package Controller
+ * @subpackage Common
+ */
+class Standard
+	extends \Aimeos\Controller\Common\Common\Import\Xml\Processor\Base
+	implements \Aimeos\Controller\Common\Common\Import\Xml\Processor\Iface
+{
+	use \Aimeos\Controller\Common\Common\Import\Xml\Traits;
+
+
+	/** controller/common/common/import/xml/processor/lists/supplier/name
+	 * Name of the lists processor implementation
+	 *
+	 * Use "Myname" if your class is named "\Aimeos\Controller\Common\Common\Import\Xml\Processor\Lists\Supplier\Myname".
+	 * The name is case-sensitive and you should avoid camel case names like "MyName".
+	 *
+	 * @param string Last part of the processor class name
+	 * @since 2019.04
+	 * @category Developer
+	 */
+
+
+	/**
+	 * Updates the given item using the data from the DOM node
+	 *
+	 * @param \Aimeos\MShop\Common\Item\Iface $item Item which should be updated
+	 * @param \DOMNode $node XML document node containing a list of nodes to process
+	 * @return \Aimeos\MShop\Common\Item\Iface Updated item
+	 */
+	public function process( \Aimeos\MShop\Common\Item\Iface $item, \DOMNode $node ) : \Aimeos\MShop\Common\Item\Iface
+	{
+		\Aimeos\MW\Common\Base::checkClass( \Aimeos\MShop\Common\Item\ListsRef\Iface::class, $item );
+
+		$context = $this->context();
+		$resource = $item->getResourceType();
+		$listItems = $item->getListItems( 'supplier', null, null, false );
+		$listManager = \Aimeos\MShop::create( $context, $resource . '/lists' );
+		$map = $this->getItems( $node->childNodes );
+
+		foreach( $node->childNodes as $node )
+		{
+			$attributes = $node->attributes;
+
+			if( $node->nodeName !== 'supplieritem' ) {
+				continue;
+			}
+
+			if( ( $attr = $attributes->getNamedItem( 'ref' ) ) === null || !isset( $map[$attr->nodeValue] ) ) {
+				continue;
+			}
+
+			$list = [];
+			$refId = $map[$attr->nodeValue]->getId();
+			$type = ( $attr = $attributes->getNamedItem( 'lists.type' ) ) !== null ? $attr->nodeValue : 'default';
+
+			if( ( $listItem = $item->getListItem( 'supplier', $type, $refId ) ) === null ) {
+				$listItem = $listManager->create();
+			} else {
+				unset( $listItems[$listItem->getId()] );
+			}
+
+			foreach( $attributes as $attrName => $attrNode ) {
+				$list[$resource . '.' . $attrName] = $attrNode->nodeValue;
+			}
+
+			$name = $resource . '.lists.config';
+			$list[$name] = ( isset( $list[$name] ) ? (array) json_decode( $list[$name] ) : [] );
+			$list[$resource . '.lists.type'] = $type;
+
+			$this->addType( $resource . '/lists/type', 'supplier', $type );
+
+			$listItem = $listItem->fromArray( $list )->setRefId( $refId );
+			$item = $item->addListItem( 'supplier', $listItem );
+		}
+
+		return $item->deleteListItems( $listItems->toArray() );
+	}
+
+
+	/**
+	 * Returns the supplier items for the given nodes
+	 *
+	 * @param \DomNodeList $nodes List of XML supplier item nodes
+	 * @return \Aimeos\MShop\Supplier\Item\Iface[] Associative list of supplier items with codes as keys
+	 */
+	protected function getItems( \DomNodeList $nodes ) : array
+	{
+		$codes = $map = [];
+		$manager = \Aimeos\MShop::create( $this->context(), 'supplier' );
+
+		foreach( $nodes as $node )
+		{
+			if( $node->nodeName === 'supplieritem' && ( $attr = $node->attributes->getNamedItem( 'ref' ) ) !== null ) {
+				$codes[$attr->nodeValue] = null;
+			}
+		}
+
+		$search = $manager->filter()->slice( 0, count( $codes ) );
+		$search->setConditions( $search->compare( '==', 'supplier.code', array_keys( $codes ) ) );
+
+		foreach( $manager->search( $search, [] ) as $item ) {
+			$map[$item->getCode()] = $item;
+		}
+
+		return $map;
+	}
+}
---
+++ controller/common/tests/Controller/Common/Common/Import/Xml/Processor/Lists/Supplier/StandardTest.php
@@ -0,0 +1,112 @@
+<?php
+
+/**
+ * @license LGPLv3, http://opensource.org/licenses/LGPL-3.0
+ * @copyright Aimeos (aimeos.org), 2021
+ */
+
+
+namespace Aimeos\Controller\Common\Common\Import\Xml\Processor\Lists\Supplier;
+
+
+class StandardTest extends \PHPUnit\Framework\TestCase
+{
+	private $context;
+	private $object;
+
+
+	protected function setUp() : void
+	{
+		$this->context = \TestHelperCntl::context();
+		$this->object = new \Aimeos\Controller\Common\Common\Import\Xml\Processor\Lists\Supplier\Standard( $this->context );
+	}
+
+
+	protected function tearDown() : void
+	{
+		unset( $this->object, $this->context );
+	}
+
+
+	public function testProcess()
+	{
+		$dom = new \DOMDocument();
+		$manager = \Aimeos\MShop::create( $this->context, 'product' );
+		$supManager = \Aimeos\MShop::create( $this->context, 'supplier' );
+
+		$refId1 = $supManager->find( 'unitSupplier001' )->getId();
+		$refId2 = $supManager->find( 'unitSupplier002' )->getId();
+
+		$dom->loadXML( '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<supplier>
+	<supplieritem ref="unitSupplier001" lists.type="default" />
+	<supplieritem ref="unitSupplier002" lists.type="test" />
+</supplier>' );
+
+		$product = $this->object->process( $manager->create(), $dom->firstChild );
+
+		$this->assertEquals( 2, count( $product->getListItems() ) );
+		$this->assertNotNull( $product->getListItem( 'supplier', 'default', $refId1 ) );
+		$this->assertNotNull( $product->getListItem( 'supplier', 'test', $refId2 ) );
+	}
+
+
+	public function testProcessUpdate()
+	{
+		$dom = new \DOMDocument();
+		$manager = \Aimeos\MShop::create( $this->context, 'product' );
+		$supManager = \Aimeos\MShop::create( $this->context, 'supplier' );
+
+		$product = $manager->create();
+		$refId1 = $supManager->find( 'unitSupplier001' )->getId();
+		$refId2 = $supManager->find( 'unitSupplier002' )->getId();
+
+		$product->addListItem( 'supplier',
+			$manager->createListItem()->setType( 'default' )->setId( 1 )->setRefId( $refId1 )
+		);
+		$product->addListItem( 'supplier',
+			$manager->createListItem()->setType( 'test' )->setId( 2 )->setRefId( $refId2 )
+		);
+
+		$dom->loadXML( '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<supplier>
+	<supplieritem ref="unitSupplier001" lists.type="test" />
+	<supplieritem ref="unitSupplier002" lists.type="default" />
+</supplier>' );
+
+		$product = $this->object->process( $product, $dom->firstChild );
+
+		$this->assertEquals( 2, count( $product->getListItems() ) );
+		$this->assertNotNull( $product->getListItem( 'supplier', 'test', $refId1 ) );
+		$this->assertNotNull( $product->getListItem( 'supplier', 'default', $refId2 ) );
+	}
+
+
+	public function testProcessDelete()
+	{
+		$dom = new \DOMDocument();
+		$manager = \Aimeos\MShop::create( $this->context, 'product' );
+		$supManager = \Aimeos\MShop::create( $this->context, 'supplier' );
+
+		$product = $manager->create();
+		$refId1 = $supManager->find( 'unitSupplier001' )->getId();
+		$refId2 = $supManager->find( 'unitSupplier002' )->getId();
+
+		$product->addListItem( 'supplier',
+			$manager->createListItem()->setType( 'default' )->setId( 1 )->setRefId( $refId1 )
+		);
+		$product->addListItem( 'supplier',
+			$manager->createListItem()->setType( 'test' )->setId( 2 )->setRefId( $refId2 )
+		);
+
+		$dom->loadXML( '<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
+<supplier>
+	<supplieritem ref="unitSupplier002" lists.type="default" />
+</supplier>' );
+
+		$product = $this->object->process( $product, $dom->firstChild );
+
+		$this->assertEquals( 1, count( $product->getListItems() ) );
+		$this->assertNotNull( $product->getListItem( 'supplier', 'default', $refId2 ) );
+	}
+}
