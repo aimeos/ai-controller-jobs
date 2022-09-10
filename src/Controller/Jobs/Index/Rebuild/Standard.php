f9bfce807309a9ac714e6a4784017f4fd6bff4f3
Aimeos <aimeos@aimeos.org>
2022-09-10 17:34:27 +0200

    Revert "Remove all cache entries with tag "product" after index rebuild"
    This reverts commit e083c2dd8e5e3ab57bcccfb06cc494031775f459.

--- src/Controller/Jobs/Index/Rebuild/Standard.php
+++ src/Controller/Jobs/Index/Rebuild/Standard.php
@@ -166,7 +166,5 @@ class Standard
 		$date = date( 'Y-m-d H:i:s' );
 
 		\Aimeos\MShop::create( $context, 'index' )->rebuild()->cleanup( $date );
-
-		$context->cache()->deleteByTags( ['product'] );
 	}
 }
