59d090737cc012d1f0cafe752930de8555cb9082
Aimeos <aimeos@aimeos.org>
2022-11-04 12:33:56 +0100

    Improved bought together job implementation

--- src/Controller/Jobs/Product/Bought/Standard.php
+++ src/Controller/Jobs/Product/Bought/Standard.php
@@ -162,10 +162,139 @@ class Standard
 	 */
 	public function run()
 	{
-		$context = $this->context();
-		$config = $context->config();
+		$ctime = $this->ctime();
+
+		if( !( $total = $this->total( $ctime ) ) ) {
+			return;
+		}
+
+		$manager = \Aimeos\MShop::create( $this->context(), 'product' );
+		$domains = $this->domains();
+		$size = $this->size();
+
+		$counts = $this->counts( $ctime );
+		$ids = $counts->keys();
+		$start = 0;
+
+		while( !( $prodIds = $ids->slice( $start, $size ) )->isEmpty() )
+		{
+			$filter = $manager->filter()->add( 'product.id', '==', $prodIds )->slice( 0, 0x7fffffff );
+			$products = $manager->search( $filter, $domains );
+
+			foreach( $counts as $id => $count )
+			{
+				if( $item = $products->get( $id ) ) {
+					$this->update( $item, $prodIds, $total, $count );
+				}
+			}
+
+			$manager->save( $products );
+			$start += $size;
+		}
+	}
 
 
+	/**
+	 * Returns the minimum confidence value for high quality suggestions
+	 *
+	 * @return float Minimum confidence value from 0 to 1
+	 */
+	protected function confidence() : float
+	{
+		/** controller/jobs/product/bought/min-confidence
+		 * Minimum confidence value for high quality suggestions
+		 *
+		 * The confidence value is used to remove low quality suggestions. Using
+		 * a confidence value of 0.95 would only suggest product combinations
+		 * that are almost always bought together. Contrary, a value of 0.1 would
+		 * yield a lot of combinations that are bought together only in very rare
+		 * cases.
+		 *
+		 * To get good product suggestions, the value should be at least above
+		 * 0.5 and the higher the value, the better the suggestions. You can
+		 * either increase the default value to get better suggestions or lower
+		 * the value to get more suggestions per product if you have only a few
+		 * ones in total.
+		 *
+		 * @param float Minimum confidence value from 0 to 1
+		 * @since 2014.09
+		 * @category Developer
+		 * @category User
+		 * @see controller/jobs/product/bought/max-items
+		 * @see controller/jobs/product/bought/min-support
+		 * @see controller/jobs/product/bought/limit-days
+		 */
+		return $this->context()->config()->get( 'controller/jobs/product/bought/min-confidence', 0.66 );
+	}
+
+
+	/**
+	 * Returns how often the product has been bought
+	 *
+	 * @param string $ctime Date of the oldest ordered product to use
+	 * @return \Aimeos\Map Map of product IDs as keys and count as values
+	 */
+	protected function counts( string $ctime ) : \Aimeos\Map
+	{
+		$manager = \Aimeos\MShop::create( $this->context(), 'order/base/product' );
+		$filter = $manager->filter()->add( 'order.base.product.ctime', '>', $ctime )->slice( 0, 0x7fffffff );
+
+		return $manager->aggregate( $filter, 'order.base.product.productid' );
+	}
+
+
+	/**
+	 * Returns the date of the oldest ordered product to use
+	 *
+	 * @return string Date of the oldest ordered product to use
+	 */
+	protected function ctime() : string
+	{
+		/** controller/jobs/product/bought/limit-days
+		 * Only use orders placed in the past within the configured number of days for calculating bought together products
+		 *
+		 * This option limits the orders that are evaluated for calculating the
+		 * bought together products. Only ordered products that were bought by
+		 * customers within the configured number of days are used.
+		 *
+		 * Limiting the orders taken into account to the last ones increases the
+		 * quality of suggestions if customer interests shifts to new products.
+		 * If you only have a few orders per month, you can also increase this
+		 * value to several years to get enough suggestions. Please keep in mind
+		 * that the more orders are evaluated, the longer the it takes to
+		 * calculate the product combinations.
+		 *
+		 * @param integer Number of days
+		 * @since 2014.09
+		 * @category User
+		 * @category Developer
+		 * @see controller/jobs/product/bought/max-items
+		 * @see controller/jobs/product/bought/min-support
+		 * @see controller/jobs/product/bought/min-confidence
+		 */
+		$days = $this->context()->config()->get( 'controller/jobs/product/bought/limit-days', 360 );
+		return date( 'Y-m-d H:i:s', time() - $days * 86400 );
+	}
+
+
+	/**
+	 * Returns the domain names to fetch for each product
+	 *
+	 * @return array Domain names to fetch for each product
+	 */
+	protected function domains() : array
+	{
+		return $this->context()->config()->get( 'mshop/product/manager/domains', [] ) + ['product'];
+	}
+
+
+	/**
+	 * Returns the maximum number of suggested items per product
+	 *
+	 * @return int Number of suggested products
+	 */
+	protected function max() : int
+	{
 		/** controller/jobs/product/bought/max-items
 		 * Maximum number of suggested items per product
 		 *
@@ -183,8 +312,93 @@ class Standard
 		 * @see controller/jobs/product/bought/min-confidence
 		 * @see controller/jobs/product/bought/limit-days
 		 */
-		$maxItems = $config->get( 'controller/jobs/product/bought/max-items', 5 );
+		return $this->context()->config()->get( 'controller/jobs/product/bought/max-items', 5 );
+	}
 
+
+	/**
+	 * Returns the relative counts for the given product IDs
+	 *
+	 * @param string $id Product ID to calculate the suggestions for
+	 * @param iterable $prodIds List of product IDs to create suggestions for
+	 * @return \Aimeos\Map Map with the product IDs as keys and the relative counts as values
+	 */
+	protected function relative( string $id, iterable $prodIds ) : \Aimeos\Map
+	{
+		$manager = \Aimeos\MShop::create( $this->context(), 'order/base/product' );
+
+		$search = $manager->filter();
+		$search->add( $search->and( [
+			$search->is( 'order.base.product.productid', '==', $prodIds ),
+			$search->is( 'order.base.product.ctime', '>', $this->ctime() ),
+			$search->is( $search->make( 'order.base.product.count', [$id] ), '==', 1 ),
+		] ) );
+
+		return $manager->aggregate( $search, 'order.base.product.productid' )->remove( $id );
+	}
+
+
+	/**
+	 * Returns the number of items processed at once
+	 *
+	 * @return int Number of items processed at once
+	 */
+	protected function size() : int
+	{
+		/** controller/jobs/product/bought/size
+		 * Number of items processed at once
+		 *
+		 * The items which are bought together are processed in batches to reduce
+		 * the time needed for associating all items. Higher numbers can improve
+		 * the speed while requiring more memory.
+		 *
+		 * @param integer Number of items processed at once
+		 * @since 2023.01
+		 * @see controller/jobs/product/bought/min-support
+		 * @see controller/jobs/product/bought/min-confidence
+		 * @see controller/jobs/product/bought/limit-days
+		 */
+		return $this->context()->config()->get( 'controller/jobs/product/bought/size', 100 );
+	}
+
+
+	/**
+	 * Returns the IDs of the suggested products.
+	 *
+	 * @param string $id Product ID to calculate the suggestions for
+	 * @param iterable $prodIds List of product IDs to create suggestions for
+	 * @param int $count Number of ordered products
+	 * @param int $total Total number of orders
+	 * @return \Aimeos\Map List of suggested product IDs
+	 */
+	protected function suggest( string $id, iterable $prodIds, int $count, int $total ) : \Aimeos\Map
+	{
+		$products = [];
+		$supportA = $count / $total;
+
+		$minSupport = $this->support();
+		$minConfidence = $this->confidence();
+
+		foreach( $this->relative( $id, $prodIds ) as $prodId => $relCnt )
+		{
+			$supportAB = $relCnt / $total;
+
+			if( $supportAB > $minSupport && ( $conf = ( $supportAB / $supportA ) ) > $minConfidence ) {
+				$products[$prodId] = $conf;
+			}
+		}
+
+		return map( $products )->arsort()->keys();
+	}
+
+
+	/**
+	 * Returns the minimum support value to sort out irrelevant combinations
+	 *
+	 * @return float Minimum support value from 0 to 1
+	 */
+	protected function support() : float
+	{
 		/** controller/jobs/product/bought/min-support
 		 * Minimum support value to sort out all irrelevant combinations
 		 *
@@ -212,160 +426,53 @@ class Standard
 		 * @see controller/jobs/product/bought/min-confidence
 		 * @see controller/jobs/product/bought/limit-days
 		 */
-		$minSupport = $config->get( 'controller/jobs/product/bought/min-support', 0.02 );
-
-		/** controller/jobs/product/bought/min-confidence
-		 * Minimum confidence value for high quality suggestions
-		 *
-		 * The confidence value is used to remove low quality suggestions. Using
-		 * a confidence value of 0.95 would only suggest product combinations
-		 * that are almost always bought together. Contrary, a value of 0.1 would
-		 * yield a lot of combinations that are bought together only in very rare
-		 * cases.
-		 *
-		 * To get good product suggestions, the value should be at least above
-		 * 0.5 and the higher the value, the better the suggestions. You can
-		 * either increase the default value to get better suggestions or lower
-		 * the value to get more suggestions per product if you have only a few
-		 * ones in total.
-		 *
-		 * @param float Minimum confidence value from 0 to 1
-		 * @since 2014.09
-		 * @category Developer
-		 * @category User
-		 * @see controller/jobs/product/bought/max-items
-		 * @see controller/jobs/product/bought/min-support
-		 * @see controller/jobs/product/bought/limit-days
-		 */
-		$minConfidence = $config->get( 'controller/jobs/product/bought/min-confidence', 0.66 );
-
-		/** controller/jobs/product/bought/limit-days
-		 * Only use orders placed in the past within the configured number of days for calculating bought together products
-		 *
-		 * This option limits the orders that are evaluated for calculating the
-		 * bought together products. Only ordered products that were bought by
-		 * customers within the configured number of days are used.
-		 *
-		 * Limiting the orders taken into account to the last ones increases the
-		 * quality of suggestions if customer interests shifts to new products.
-		 * If you only have a few orders per month, you can also increase this
-		 * value to several years to get enough suggestions. Please keep in mind
-		 * that the more orders are evaluated, the longer the it takes to
-		 * calculate the product combinations.
-		 *
-		 * @param integer Number of days
-		 * @since 2014.09
-		 * @category User
-		 * @category Developer
-		 * @see controller/jobs/product/bought/max-items
-		 * @see controller/jobs/product/bought/min-support
-		 * @see controller/jobs/product/bought/min-confidence
-		 */
-		$days = $config->get( 'controller/jobs/product/bought/limit-days', 180 );
-		$date = date( 'Y-m-d H:i:s', time() - $days * 86400 );
-
-		$domains = [
-			'attribute', 'catalog', 'media', 'media/property', 'price',
-			'product', 'product/property', 'supplier', 'text'
-		];
-
-
-		$manager = \Aimeos\MShop::create( $context, 'product' );
-		$baseManager = \Aimeos\MShop::create( $context, 'order/base' );
-		$baseProductManager = \Aimeos\MShop::create( $context, 'order/base/product' );
-
-		$search = $baseProductManager->filter()->add( 'order.base.product.ctime', '>', $date );
-		$filter = $baseManager->filter()->add( 'order.base.ctime', '>', $date )->slice( 0, 0 );
-
-		$start = $total = 0;
-		$baseManager->search( $filter, [], $total )->all();
-
-		if( !$total ) {
-			return;
-		}
-
-		do
-		{
-			$counts = $baseProductManager->aggregate( $search, 'order.base.product.productid' );
-			$prodIds = $counts->keys()->all();
-
-			$filter = $manager->filter()->add( 'product.id', '==', $prodIds )->slice( 0, 0x7fffffff );
-			$products = $manager->search( $filter, $domains );
-
-			foreach( $counts as $id => $count )
-			{
-				if( ( $item = $products->get( $id ) ) === null ) {
-					continue;
-				}
-
-				$listItems = $item->getListItems( 'product', 'bought-together' );
-
-				if( $count / $total > $minSupport )
-				{
-					$productIds = $this->getSuggestions( $id, $prodIds, $count, $total, $maxItems,
-						$minSupport, $minConfidence, $date );
-
-					foreach( $productIds as $pid )
-					{
-						$litem = $item->getListItem( 'product', 'bought-together', $pid, false ) ?: $manager->createListItem();
-						$item->addListItem( 'product', $litem->setType( 'bought-together' )->setRefId( $pid ) );
-						$listItems->remove( $litem->getId() );
-					}
-				}
-
-				$item->deleteListItems( $listItems );
-			}
-
-			$manager->save( $products );
-
-			$count = count( $counts );
-			$start += $count;
-			$search->slice( $start );
-		}
-		while( $count >= $search->getLimit() );
+		return $this->context()->config()->get( 'controller/jobs/product/bought/min-support', 0.02 );
 	}
 
 
 	/**
-	 * Returns the IDs of the suggested products.
+	 * Returns the total number of orders available
 	 *
-	 * @param string $id Product ID to calculate the suggestions for
-	 * @param string[] $prodIds List of product IDs to create suggestions for
+	 * @param string $ctime Time of the oldest order to use
+	 * @return int Total number of orders
+	 */
+	protected function total( string $ctime ) : int
+	{
+		$total = 0;
+
+		$manager = \Aimeos\MShop::create( $this->context(), 'order/base' );
+		$filter = $manager->filter()->add( 'order.base.ctime', '>', $ctime )->slice( 0, 0 );
+		$manager->search( $filter, [], $total )->all();
+
+		return $total;
+	}
+
+
+	/**
+	 * Updates the products bought together for the given item
+	 *
+	 * @param \Aimeos\MShop\Product\Item\Iface $item Product item to update
+	 * @param iterable $prodIds List of product IDs to create suggestions for
 	 * @param int $count Number of ordered products
 	 * @param int $total Total number of orders
-	 * @param int $maxItems Maximum number of suggestions
-	 * @param float $minSupport Minium support value for calculating the suggested products
-	 * @param float $minConfidence Minium confidence value for calculating the suggested products
-	 * @param string $date Date in YYYY-MM-DD HH:mm:ss format after which orders should be used for calculations
-	 * @return \Aimeos\Map List of suggested product IDs
+	 * return \Aimeos\MShop\Product\Item\Iface Updated product item
 	 */
-	protected function getSuggestions( string $id, array $prodIds, int $count, int $total, int $maxItems,
-		float $minSupport, float $minConfidence, string $date ) : \Aimeos\Map
+	protected function update( \Aimeos\MShop\Product\Item\Iface $item, iterable $prodIds, int $count, int $total ) : \Aimeos\MShop\Product\Item\Iface
 	{
-		$manager = \Aimeos\MShop::create( $this->context(), 'order/base/product' );
+		$listItems = $item->getListItems( 'product', 'bought-together' );
 
-		$search = $manager->filter();
-		$search->add( $search->and( [
-			$search->is( 'order.base.product.productid', '==', $prodIds ),
-			$search->is( 'order.base.product.ctime', '>', $date ),
-			$search->is( $search->make( 'order.base.product.count', [(string) $id] ), '==', 1 ),
-		] ) );
-		$relativeCounts = $manager->aggregate( $search, 'order.base.product.productid' );
-
-
-		unset( $relativeCounts[$id] );
-		$supportA = $count / $total;
-		$products = [];
-
-		foreach( $relativeCounts as $prodId => $relCnt )
+		if( $count / $total > $this->support() )
 		{
-			$supportAB = $relCnt / $total;
+			$productIds = $this->suggest( $item->getId(), $prodIds, $count, $total )->slice( 0, $this->max() );
 
-			if( $supportAB > $minSupport && ( $conf = ( $supportAB / $supportA ) ) > $minConfidence ) {
-				$products[$prodId] = $conf;
+			foreach( $productIds as $pid )
+			{
+				$litem = $item->getListItem( 'product', 'bought-together', $pid, false ) ?: $manager->createListItem();
+				$item->addListItem( 'product', $litem->setType( 'bought-together' )->setRefId( $pid ) );
+				$listItems->remove( $litem->getId() );
 			}
 		}
 
-		return map( $products )->arsort()->slice( 0, $maxItems, true )->keys();
+		return $item->deleteListItems( $listItems );
 	}
 }
