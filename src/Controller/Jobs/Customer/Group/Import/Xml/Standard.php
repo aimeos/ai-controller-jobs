b89f32fd1580ffd798f29f6ef7628d0488ff3167
Aimeos <aimeos@aimeos.org>
2022-12-15 12:51:29 +0100

    Improved customer group XML importer

--- src/Controller/Jobs/Customer/Group/Import/Xml/Standard.php
+++ src/Controller/Jobs/Customer/Group/Import/Xml/Standard.php
@@ -52,7 +52,6 @@ class Standard
 	 *
 	 * @param string Last part of the class name
 	 * @since 2019.04
-	 * @category Developer
 	 */
 
 	/** controller/jobs/customer/import/xml/decorators/excludes
@@ -75,7 +74,6 @@ class Standard
 	 *
 	 * @param array List of decorator names
 	 * @since 2019.04
-	 * @category Developer
 	 * @see controller/jobs/common/decorators/default
 	 * @see controller/jobs/customer/import/xml/decorators/global
 	 * @see controller/jobs/customer/import/xml/decorators/local
@@ -99,7 +97,6 @@ class Standard
 	 *
 	 * @param array List of decorator names
 	 * @since 2019.04
-	 * @category Developer
 	 * @see controller/jobs/common/decorators/default
 	 * @see controller/jobs/customer/import/xml/decorators/excludes
 	 * @see controller/jobs/customer/import/xml/decorators/local
@@ -125,7 +122,6 @@ class Standard
 	 *
 	 * @param array List of decorator names
 	 * @since 2019.04
-	 * @category Developer
 	 * @see controller/jobs/common/decorators/default
 	 * @see controller/jobs/customer/import/xml/decorators/excludes
 	 * @see controller/jobs/customer/import/xml/decorators/global
@@ -165,26 +161,8 @@ class Standard
 	public function run()
 	{
 		$context = $this->context();
-		$config = $context->config();
 		$logger = $context->logger();
-
-		/** controller/jobs/customer/group/import/xml/location
-		 * File or directory where the content is stored which should be imported
-		 *
-		 * You need to configure the XML file or directory with the XML files that
-		 * should be imported. It should be an absolute path to be sure but can be
-		 * relative path if you absolutely know from where the job will be executed
-		 * from.
-		 *
-		 * @param string Absolute file or directory path
-		 * @since 2019.04
-		 * @category Developer
-		 * @category User
-		 * @see controller/jobs/customer/group/import/xml/container/type
-		 * @see controller/jobs/customer/group/import/xml/container/content
-		 * @see controller/jobs/customer/group/import/xml/container/options
-		 */
-		$location = $config->get( 'controller/jobs/customer/group/import/xml/location' );
+		$location = $this->location();
 
 		try
 		{
@@ -237,17 +215,12 @@ class Standard
 
 
 	/**
-	 * Imports the XML file given by its path
+	 * Returns the directory for storing imported files
 	 *
-	 * @param string $filename Absolute or relative path to the XML file
+	 * @return string Directory for storing imported files
 	 */
-	protected function import( string $filename )
+	protected function backup() : string
 	{
-		$context = $this->context();
-		$config = $context->config();
-		$logger = $context->logger();
-
-
 		/** controller/jobs/customer/group/import/xml/backup
 		 * Name of the backup for sucessfully imported files
 		 *
@@ -271,34 +244,27 @@ class Standard
 		 *
 		 * @param integer Name of the backup file, optionally with date/time placeholders
 		 * @since 2019.04
-		 * @category Developer
 		 * @see controller/jobs/customer/group/import/xml/domains
+		 * @see controller/jobs/customer/group/import/xml/location
 		 * @see controller/jobs/customer/group/import/xml/max-query
 		 */
-		$backup = $config->get( 'controller/jobs/customer/group/import/xml/backup' );
-
-		/** controller/jobs/customer/group/import/xml/max-query
-		 * Maximum number of XML nodes processed at once
-		 *
-		 * Processing and fetching several customer group items at once speeds up importing
-		 * the XML files. The more items can be processed at once, the faster the
-		 * import. More items also increases the memory usage of the importer and
-		 * thus, this parameter should be low enough to avoid reaching the memory
-		 * limit of the PHP process.
-		 *
-		 * @param integer Number of XML nodes
-		 * @since 2019.04
-		 * @category Developer
-		 * @category User
-		 * @see controller/jobs/customer/group/import/xml/domains
-		 * @see controller/jobs/customer/group/import/xml/backup
-		 */
-		$maxquery = $config->get( 'controller/jobs/customer/group/import/xml/max-query', 1000 );
+		return (string) $this->context()->config()->get( 'controller/jobs/customer/group/import/xml/backup' );
+	}
 
 
+	/**
+	 * Imports the XML file given by its path
+	 *
+	 * @param string $filename Absolute or relative path to the XML file
+	 */
+	protected function import( string $filename )
+	{
 		$slice = 0;
 		$nodes = [];
+
 		$xml = new \XMLReader();
+		$maxquery = $this->max();
+		$logger = $this->context()->logger();
 
 		if( $xml->open( $filename, LIBXML_COMPACT | LIBXML_PARSEHUGE ) === false ) {
 			throw new \Aimeos\Controller\Jobs\Exception( sprintf( 'No XML file "%1$s" found', $filename ) );
@@ -320,7 +286,7 @@ class Standard
 
 				if( $slice++ >= $maxquery )
 				{
-					$this->importNodes( $nodes, [] );
+					$this->importNodes( $nodes );
 					unset( $nodes );
 					$nodes = [];
 					$slice = 0;
@@ -328,7 +294,7 @@ class Standard
 			}
 		}
 
-		$this->importNodes( $nodes, [] );
+		$this->importNodes( $nodes );
 		unset( $nodes );
 
 		foreach( $this->getProcessors() as $proc ) {
@@ -337,7 +303,7 @@ class Standard
 
 		$logger->info( sprintf( 'Finished customer group import from file "%1$s"', $filename ), 'import/xml/customer/group' );
 
-		if( !empty( $backup ) && @rename( $filename, $backup = \Aimeos\Base\Str::strtime( $backup ) ) === false )
+		if( !empty( $backup = $this->backup() ) && @rename( $filename, $backup = \Aimeos\Base\Str::strtime( $backup ) ) === false )
 		{
 			$msg = sprintf( 'Unable to move imported file "%1$s" to "%2$s"', $filename, $backup );
 			throw new \Aimeos\Controller\Jobs\Exception( $msg );
@@ -348,10 +314,9 @@ class Standard
 	/**
 	 * Imports the given DOM nodes
 	 *
-	 * @param \DomElement[] $nodes List of nodes to import
 	 * @param string[] $ref List of domain names whose referenced items will be updated in the customer group items
 	 */
-	protected function importNodes( array $nodes, array $ref )
+	protected function importNodes( array $nodes )
 	{
 		$codes = $map = [];
 
@@ -366,7 +331,7 @@ class Standard
 		$search = $manager->filter()->slice( 0, count( $codes ) );
 		$search->setConditions( $search->compare( '==', 'customer.group.code', array_keys( $codes ) ) );
 
-		foreach( $manager->search( $search, $ref ) as $item ) {
+		foreach( $manager->search( $search ) as $item ) {
 			$map[$item->getCode()] = $item;
 		}
 
@@ -380,6 +345,57 @@ class Standard
 
 			$manager->save( $item );
 		}
+	}
+
+
+	/**
+	 * Returns the path to the directory with the XML file
+	 *
+	 * @return string Path to the directory with the XML file
+	 */
+	protected function location() : string
+	{
+		/** controller/jobs/customer/group/import/xml/location
+		 * File or directory where the content is stored which should be imported
+		 *
+		 * You need to configure the XML file or directory with the XML files that
+		 * should be imported. It should be an absolute path to be sure but can be
+		 * relative path if you absolutely know from where the job will be executed
+		 * from.
+		 *
+		 * @param string Relative path to the XML files
+		 * @since 2019.04
+		 * @see controller/jobs/customer/group/import/xml/backup
+		 * @see controller/jobs/customer/group/import/xml/domains
+		 * @see controller/jobs/customer/group/import/xml/max-query
+		 */
+		return (string) $this->context()->config()->get( 'controller/jobs/customer/group/import/xml/location', 'customergroup' );
+	}
+
+
+	/**
+	 * Returns the maximum number of XML nodes processed at once
+	 *
+	 * @return int Maximum number of XML nodes
+	 */
+	protected function max() : int
+	{
+		/** controller/jobs/customer/group/import/xml/max-query
+		 * Maximum number of XML nodes processed at once
+		 *
+		 * Processing and fetching several attribute items at once speeds up importing
+		 * the XML files. The more items can be processed at once, the faster the
+		 * import. More items also increases the memory usage of the importer and
+		 * thus, this parameter should be low enough to avoid reaching the memory
+		 * limit of the PHP process.
+		 *
+		 * @param integer Number of XML nodes
+		 * @since 2019.04
+		 * @see controller/jobs/customer/group/import/xml/domains
+		 * @see controller/jobs/customer/group/import/xml/location
+		 * @see controller/jobs/customer/group/import/xml/backup
+		 */
+		return $this->context()->config()->get( 'controller/jobs/customer/group/import/xml/max-query', 100 );
 	}
 
 
