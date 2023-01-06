88d223bd33946cf107a540cabc7cf3ab0181046c
Aimeos <aimeos@aimeos.org>
2023-01-06 16:36:07 +0100

    Simplified subscription CSV export by using a template

--- src/Controller/Jobs/Subscription/Export/Csv/Standard.php
+++ src/Controller/Jobs/Subscription/Export/Csv/Standard.php
@@ -18,7 +18,7 @@ namespace Aimeos\Controller\Jobs\Subscription\Export\Csv;
  * @subpackage Jobs
  */
 class Standard
-	extends \Aimeos\Controller\Jobs\Subscription\Export\Csv\Base
+	extends \Aimeos\Controller\Jobs\Base
 	implements \Aimeos\Controller\Jobs\Iface
 {
 	/** controller/jobs/subscription/export/csv/name
@@ -158,45 +158,41 @@ class Standard
 	public function run()
 	{
 		$context = $this->context();
-		$config = $context->config();
-		$logger = $context->logger();
-		$mappings = $this->getDefaultMapping();
+		$mq = $context->queue( 'mq-admin', 'subscription-export' );
+
+		while( $msg = $mq->get() )
+		{
+			try
+			{
+				$body = $msg->getBody();
+
+				if( ( $data = json_decode( $body, true ) ) === null ) {
+					throw new \Aimeos\Controller\Jobs\Exception( sprintf( 'Invalid message: %1$s', $body ) );
+				}
+
+				$this->export( $data );
+			}
+			catch( \Exception $e )
+			{
+				$str = 'Subscription export error: ' . $e->getMessage() . "\n" . $e->getTraceAsString();
+				$context->logger()->error( $str, 'subscription/export/csv' );
+			}
+
+			$mq->del( $msg );
+		}
+	}
 
 
-		/** controller/common/subscription/export/csv/mapping
-		 * List of mappings between the position in the CSV file and item keys
-		 *
-		 * The exporter has to know which data is at which position in the CSV
-		 * file. Therefore, you need to specify a mapping between each position
-		 * and the MShop domain item key (e.g. "subscription.type") it represents.
-		 *
-		 * These mappings are grouped together by their processor names, which
-		 * are responsible for exporting the data, e.g. all mappings in "invoice"
-		 * will be managed by the invoice processor while the mappings in
-		 * "product" will be exported by the product processor.
-		 *
-		 * @param array Associative list of processor names and lists of key/position pairs
-		 * @since 2018.04
-		 * @see controller/common/subscription/export/csv/max-size
-		 */
-		$mappings = $config->get( 'controller/common/subscription/export/csv/mapping', $mappings );
-
-		/** controller/jobs/subscription/export/csv/mapping
-		 * List of mappings between the position in the CSV file and item keys
-		 *
-		 * This configuration setting overwrites the shared option
-		 * "controller/common/subscription/export/csv/mapping" if you need a
-		 * specific setting for the job controller. Otherwise, you should
-		 * use the shared option for consistency.
-		 *
-		 * @param array Associative list of processor names and lists of key/position pairs
-		 * @since 2018.04
-		 * @see controller/common/subscription/export/csv/max-size
-		 */
-		$mappings = $config->get( 'controller/jobs/subscription/export/csv/mapping', $mappings );
-
-
-		/** controller/common/subscription/export/csv/max-size
+	/**
+	 * Initializes the search criteria
+	 *
+	 * @param \Aimeos\Base\Criteria\Iface $criteria New criteria object
+	 * @param array $msg Message data
+	 * @return \Aimeos\Base\Criteria\Iface Initialized criteria object
+	 */
+	protected function criteria( \Aimeos\Base\Criteria\Iface $criteria, array $msg ) : \Aimeos\Base\Criteria\Iface
+	{
+		/** controller/jobs/subscription/export/csv/max-size
 		 * Maximum number of CSV rows to export at once
 		 *
 		 * It's more efficient to read and export more than one row at a time
@@ -207,199 +203,44 @@ class Standard
 		 * export speed.
 		 *
 		 * @param integer Number of rows
-		 * @since 2018.04
-		 * @see controller/common/subscription/export/csv/mapping
+		 * @since 2023.04
 		 */
-		$maxcnt = (int) $config->get( 'controller/common/subscription/export/csv/max-size', 1000 );
+		$size = (int) $this->context()->config()->get( 'controller/jobs/subscription/export/csv/max-size', 1000 );
 
-
-		$processors = $this->getProcessors( $mappings );
-		$mq = $context->queue( 'mq-admin', 'subscription-export' );
-
-		while( ( $msg = $mq->get() ) !== null )
-		{
-			try
-			{
-				if( ( $data = json_decode( $msg->getBody(), true ) ) === null ) {
-					throw new \Aimeos\Controller\Jobs\Exception( sprintf( 'Invalid message: %1$s', $msg->getBody() ) );
-				}
-
-				$this->export( $processors, $data, $maxcnt );
-			}
-			catch( \Exception $e )
-			{
-				$msg = 'Subscription export error: ' . $e->getMessage() . "\n" . $e->getTraceAsString();
-				$logger->error( $msg, 'export/csv/subscription' );
-			}
-
-			$mq->del( $msg );
-		}
+		return $criteria->add( $criteria->parse( $msg['filter'] ?? [] ) )->order( $msg['sort'] ?? [] )->slice( 0, $size );
 	}
 
 
 	/**
-	 * Creates a new job entry for the exported file
+	 * Exports the orders
 	 *
-	 * @param \Aimeos\MShop\ContextIface $context Context item
-	 * @param string $path Absolute path to the exported file
-	 */
-	protected function addJob( \Aimeos\MShop\ContextIface $context, string $path )
-	{
-		$manager = \Aimeos\MAdmin::create( $context, 'job' );
-		$item = $manager->create()->setPath( $path )->setLabel( $path );
-		$manager->save( $item, false );
-	}
-
-
-	/**
-	 * Opens and returns the container which includes the subscription data
-	 *
-	 * @return \Aimeos\MW\Container\Iface Container object
-	 */
-	protected function getContainer() : \Aimeos\MW\Container\Iface
-	{
-		$config = $this->context()->config();
-
-		/** controller/jobs/subscription/export/csv/location
-		 * Temporary file or directory where the content is stored which should be exported
-		 *
-		 * The path can point to any supported container format as long as the
-		 * content is in CSV format, e.g.
-		 *
-		 * * Directory container / CSV file
-		 * * Zip container / compressed CSV file
-		 *
-		 * @param string Absolute file or directory path
-		 * @since 2018.04
-		 * @see controller/jobs/subscription/export/csv/container/type
-		 * @see controller/jobs/subscription/export/csv/container/content
-		 * @see controller/jobs/subscription/export/csv/container/options
-		 */
-		$location = $config->get( 'controller/jobs/subscription/export/csv/location', sys_get_temp_dir() );
-
-		/** controller/jobs/subscription/export/csv/container/type
-		 * Nave of the container type to read the data from
-		 *
-		 * The container type tells the exporter how it should retrieve the data.
-		 * There are currently three container types that support the necessary
-		 * CSV content:
-		 *
-		 * * Directory
-		 * * Zip
-		 *
-		 * @param string Container type name
-		 * @since 2015.05
-		 * @see controller/jobs/subscription/export/csv/location
-		 * @see controller/jobs/subscription/export/csv/container/content
-		 * @see controller/jobs/subscription/export/csv/container/options
-		 */
-		$container = $config->get( 'controller/jobs/subscription/export/csv/container/type', 'Directory' );
-
-		/** controller/jobs/subscription/export/csv/container/content
-		 * Name of the content type inside the container to read the data from
-		 *
-		 * The content type must always be a CSV-like format and there are
-		 * currently two format types that are supported:
-		 *
-		 * * CSV
-		 *
-		 * @param array Content type name
-		 * @since 2015.05
-		 * @see controller/jobs/subscription/export/csv/location
-		 * @see controller/jobs/subscription/export/csv/container/type
-		 * @see controller/jobs/subscription/export/csv/container/options
-		 */
-		$content = $config->get( 'controller/jobs/subscription/export/csv/container/content', 'CSV' );
-
-		/** controller/jobs/subscription/export/csv/container/options
-		 * List of file container options for the subscription export files
-		 *
-		 * Some container/content type allow you to hand over additional settings
-		 * for configuration. Please have a look at the article about
-		 * {@link http://aimeos.org/docs/Developers/Utility/Create_and_read_files container/content files}
-		 * for more information.
-		 *
-		 * @param array Associative list of option name/value pairs
-		 * @since 2015.05
-		 * @see controller/jobs/subscription/export/csv/location
-		 * @see controller/jobs/subscription/export/csv/container/content
-		 * @see controller/jobs/subscription/export/csv/container/type
-		 */
-		$options = $config->get( 'controller/jobs/subscription/export/csv/container/options', [] );
-
-		return \Aimeos\MW\Container\Factory::getContainer( $location, $container, $content, $options );
-	}
-
-
-	/**
-	 * Exports the subscriptions and returns the exported file name
-	 *
-	 * @param Aimeos\Controller\Common\Subscription\Export\Csv\Processor\Iface[] List of processor objects
 	 * @param array $msg Message data passed from the frontend
-	 * @param int $maxcnt Maximum number of retrieved subscriptions at once
 	 */
-	protected function export( array $processors, array $msg, int $maxcnt )
+	protected function export( array $msg )
 	{
+		if( ( $fh = tmpfile() ) === false ) {
+			throw new \Aimeos\Controller\Jobs\Exception( 'Unable to create temporary file' );
+		}
+
+		$path = $this->path();
 		$lcontext = $this->getLocaleContext( $msg );
 
-		/** controller/jobs/subscription/export/csv/collapse
-		 * Collapse all lines in the subscription export on only one line
-		 *
-		 * By default, the subscription export will contain several lines for each
-		 * subscription, e.g. for the product, the addresses, the delivery and payment
-		 * services as well as the invoice data. You can merge them into one line using
-		 * this configuration setting.
-		 *
-		 * @param bool True to collapse all lines of one subscription, false for separate lines
-		 * @since 2020.07
-		 */
-		$collapse = $lcontext->config()->get( 'controller/jobs/subscription/export/csv/collapse', false );
-
 		$manager = \Aimeos\MShop::create( $lcontext, 'subscription' );
+		$cursor = $manager->cursor( $this->criteria( $manager->filter( false, true ), $msg ) );
 
-		$container = $this->getContainer();
-		$content = $container->create( 'subscription-export_' . date( 'Y-m-d_H-i-s' ) );
-		$search = $this->initCriteria( $manager->filter( false, true )->slice( 0, 0x7fffffff ), $msg );
-		$start = 0;
-
-		do
+		while( $items = $manager->iterate( $cursor, ['order', 'order/address', 'order/product'] ) )
 		{
-			$baseIds = [];
-			$search->slice( $start, $maxcnt );
-			$items = $manager->search( $search, ['order', 'order/address', 'order/product'] );
-
-			foreach( $items as $id => $item )
-			{
-				$lines = [];
-
-				foreach( $processors as $type => $processor )
-				{
-					foreach( $processor->process( $item ) as $line )
-					{
-						if( !$collapse ) {
-							$lines[] = [0 => $type, 1 => $id] + $line;
-						} else {
-							$lines[0] = array_replace( $lines[0] ?? [], $line );
-						}
-					}
-				}
-
-				foreach( $lines as $line ) {
-					$content->add( $line );
-				}
+			if( fwrite( $fh, $this->render( $items ) ) === false ) {
+				throw new \Aimeos\Controller\Jobs\Exception( 'Unable to add data to temporary file' );
 			}
-
-			$count = count( $items );
-			$start += $count;
 		}
-		while( $count === $search->getLimit() );
 
-		$path = $content->getResource();
-		$container->add( $content );
-		$container->close();
+		rewind( $fh );
+		$lcontext->fs( 'fs-export' )->writes( $path, $fh );
+		fclose( $fh );
 
-		$path = $this->moveFile( $lcontext, $path );
-		$this->addJob( $lcontext, $path );
+		$manager = \Aimeos\MAdmin::create( $lcontext, 'job' );
+		$manager->save( $manager->create()->setPath( $path )->setLabel( $path ), false );
 	}
 
 
@@ -422,31 +263,43 @@ class Standard
 
 
 	/**
-	 * Initializes the search criteria
+	 * Returns the relative path the subscriptions should be exported to
 	 *
-	 * @param \Aimeos\Base\Criteria\Iface $criteria New criteria object
-	 * @param array $msg Message data
-	 * @return \Aimeos\Base\Criteria\Iface Initialized criteria object
+	 * @return string Relativ path to the export file
 	 */
-	protected function initCriteria( \Aimeos\Base\Criteria\Iface $criteria, array $msg ) : \Aimeos\Base\Criteria\Iface
+	protected function path() : string
 	{
-		return $criteria->add( $criteria->parse( $msg['filter'] ?? [] ) )->order( $msg['sort'] ?? [] );
+		/** controller/jobs/subscription/export/csv/path
+		 * Relativ path to the export file
+		 *
+		 * It's more efficient to read and export more than one row at a time
+		 * to speed up the export. Usually, the bigger the chunk that is exported
+		 * at once, the less time the exporter will need. The downside is that
+		 * the amount of memory required by the export process will increase as
+		 * well. Therefore, it's a trade-off between memory consumption and
+		 * export speed.
+		 *
+		 * @param string Relativ path with placeholders
+		 * @since 2023.04
+		 */
+		$path = 'subscription-export_%Y-%m-%d_%H-%i-%s';
+		$path = $this->context()->config()->get( 'controller/jobs/subscription/export/csv/path', $path );
+
+		return \Aimeos\Base\Str::strtime( $path );
 	}
 
 
 	/**
-	 * Moves the exported file to the final storage
+	 * Creates the CSV file for the given subscriptions
 	 *
-	 * @param \Aimeos\MShop\ContextIface $context Context item
-	 * @param string $path Absolute path to the exported file
-	 * @return string Relative path of the file in the storage
+	 * @param \Aimeos\MShop\Subscription\Item\Iface[] $items List of subscription items to export
+	 * @return string Generated CSV
 	 */
-	protected function moveFile( \Aimeos\MShop\ContextIface $context, string $path ) : string
+	protected function render( iterable $items ) : string
 	{
-		$filename = basename( $path );
-		$context->fs( 'fs-admin' )->writef( $filename, $path );
+		$context = $this->context();
+		$template = $context->config()->get( 'controller/jobs/subscription/export/csv/template', 'subscription/export/csv/body' );
 
-		unlink( $path );
-		return $filename;
+		return $context->view()->assign( ['items' => $items] )->render( $template );
 	}
 }
---
+++ templates/subscription/export/csv/body.php
@@ -0,0 +1,106 @@
+<?php
+
+$subscriptionFcn = function( \Aimeos\MShop\Subscription\Item\Iface $item ) {
+	return [
+		'subscription',
+		$item->getId(),
+		$item->getInterval(),
+		$item->getDateNext(),
+		$item->getDateEnd(),
+		$item->getPeriod(),
+		$item->getStatus(),
+		$item->getOrderId(),
+	];
+};
+
+$addressFcn = function( \Aimeos\MShop\Order\Item\Address\Iface $item ) {
+	return [
+		'address',
+		$item->getParentId(),
+		$item->getType(),
+		$item->getSalutation(),
+		$item->getCompany(),
+		$item->getVatID(),
+		$item->getTitle(),
+		$item->getFirstName(),
+		$item->getLastName(),
+		$item->getAddress1(),
+		$item->getAddress2(),
+		$item->getAddress3(),
+		$item->getPostal(),
+		$item->getCity(),
+		$item->getState(),
+		$item->getCountryId(),
+		$item->getLanguageId(),
+		$item->getTelephone(),
+		$item->getTelefax(),
+		$item->getEmail(),
+		$item->getWebsite(),
+		$item->getLongitude(),
+		$item->getLatitude(),
+	];
+};
+
+$productFcn = function( \Aimeos\MShop\Order\Item\Product\Iface $item ) {
+	$list = [
+		'product',
+		$item->getParentId(),
+		$item->getType(),
+		$item->getStockType(),
+		$item->getVendor(),
+		$item->getProductCode(),
+		$item->getScale(),
+		$item->getQuantity(),
+		$item->getQuantityOpen(),
+		$item->getName(),
+		$item->getDescription(),
+		$item->getMediaUrl(),
+		$item->getPrice()->getValue(),
+		$item->getPrice()->getCosts(),
+		$item->getPrice()->getRebate(),
+		$item->getPrice()->getTaxrate(),
+		$item->getPrice()->getTaxvalue(),
+		$item->getPrice()->getTaxflag(),
+		$item->getStatusPayment(),
+		$item->getStatusDelivery(),
+		$item->getTimeframe(),
+		$item->getPosition(),
+		$item->getNotes(),
+	];
+
+	if( $attr = $item->getAttributeItems()->first() )
+	{
+		$list[] = $attr->getType();
+		$list[] = $attr->getCode();
+		$list[] = $attr->getName();
+		$list[] = $attr->getValue();
+	}
+
+	return $list;
+};
+
+
+foreach( $this->get( 'items', [] ) as $item )
+{
+	echo '"' . join( '","', $subscriptionFcn( $item ) ) . '"' . "\n";
+
+    if( $orderItem = $item->getOrderItem() )
+    {
+        foreach( $orderItem->getAddress( 'payment' ) as $address ) {
+            echo '"' . join( '","', $addressFcn( $address ) ) . '"' . "\n";
+        }
+
+        foreach( $orderItem->getAddress( 'delivery' ) as $address ) {
+            echo '"' . join( '","', $addressFcn( $address ) ) . '"' . "\n";
+        }
+
+        foreach( $orderItem->getProducts() as $product )
+        {
+            echo '"' . join( '","', $productFcn( $product ) ) . '"' . "\n";
+
+            foreach( $product->getProducts() as $subProduct ) {
+                echo '"' . join( '","', $productFcn( $subProduct ) ) . '"' . "\n";
+            }
+        }
+    }
+}
