3a9d74d4ea739e84104c30c96043d9cad46aa4dd
Aimeos <aimeos@aimeos.org>
2023-12-09 10:09:26 +0100

    Rewrote product export job to be able to use remote file systems

--- src/Controller/Jobs/Product/Export/Standard.php
+++ src/Controller/Jobs/Product/Export/Standard.php
@@ -157,19 +157,133 @@ class Standard
 	 */
 	public function run()
 	{
-		$container = $this->createContainer();
 		$this->export( $container, false );
-		$container->close();
 	}
 
 
 	/**
-	 * Adds the given products to the content object for the site map file
+	 * Exports the products
 	 *
-	 * @param \Aimeos\MW\Container\Content\Iface $content File content object
-	 * @param \Aimeos\Map $items List of product items implementing \Aimeos\MShop\Product\Item\Iface
+	 * @param bool|null $default True to filter exported products by default criteria
+	 * @return array List of file names
 	 */
-	protected function addItems( \Aimeos\MW\Container\Content\Iface $content, \Aimeos\Map $items )
+	protected function export( ?bool $default = true ) : array
+	{
+		$manager = \Aimeos\MShop::create( $this->context(), 'product' );
+		$filter = $manager->filter( $default )->order( 'product.id' )->slice( 0, $this->max() );
+		$cursor = $manager->cursor( $filter );
+
+		$domains = $this->domains();
+		$fs = $this->fs();
+
+		$filenum = 1;
+		$files = [];
+
+		while( $items = $manager->iterate( $cursor, $domains ) )
+		{
+			$filename = $this->call( 'filename', $filenum++ );
+			$fs->write( $filename, $this->render( $items ) );
+			$files[] = $filename;
+		}
+
+		return $files;
+	}
+
+
+	/**
+	 * Returns the domain names whose items should be exported too
+	 *
+	 * @return array List of domain names
+	 */
+	protected function domains() : array
+	{
+		/** controller/jobs/product/export/domains
+		 * List of associated items from other domains that should be exported too
+		 *
+		 * Products consist not only of the base data but also of texts, media
+		 * files, prices, attrbutes and other details. Those information is
+		 * associated to the products via their lists. Using the "domains" option
+		 * you can make more or less associated items available in the template.
+		 *
+		 * @param array List of domain names
+		 * @since 2015.01
+		 * @see controller/jobs/product/export/filename
+		 * @see controller/jobs/product/export/max-items
+		 */
+		$default = ['attribute', 'media', 'price', 'product', 'text'];
+
+		return $this->context()->config()->get( 'controller/jobs/product/export/domains', $default );
+	}
+
+
+	/**
+	 * Returns the file name for the new content file
+	 *
+	 * @param int $number Current file number
+	 * @return string New file name
+	 */
+	protected function filename( int $number ) : string
+	{
+		/** controller/jobs/product/export/filename
+		 * Template for the generated file names
+		 *
+		 * The generated export files will be named according to the given
+		 * string which can contain two place holders: The number of the
+		 * exported product and the ISO date/time when the file was created.
+		 *
+		 * @param string File name template
+		 * @since 2018.04
+		 * @see controller/jobs/product/export/max-items
+		 * @see controller/jobs/product/export/domains
+		 */
+		$name = $this->context()->config()->get( 'controller/jobs/product/export/filename', 'aimeos-products-%1$d_%2$s.xml' );
+
+		return sprintf( $name, $number, date( 'Y-m-d_H:i:s' ) );
+	}
+
+
+	/**
+	 * Returns the file system for storing the exported files
+	 *
+	 * @return \Aimeos\Base\Filesystem\Iface File system to store files to
+	 */
+	protected function fs() : \Aimeos\Base\Filesystem\Iface
+	{
+		return $this->context()->fs( 'fs-export' );
+	}
+
+
+	/**
+	 * Returns the maximum number of exported products per file
+	 *
+	 * @return int Maximum number of exported products per file
+	 */
+	protected function max() : int
+	{
+		/** controller/jobs/product/export/max-items
+		 * Maximum number of exported products per file
+		 *
+		 * Limits the number of exported products per file as the memory
+		 * consumption of processing big files is rather high. Splitting
+		 * the data into several files that can also be processed in
+		 * parallel is able to speed up importing the files again.
+		 *
+		 * @param integer Number of products entries per file
+		 * @since 2015.01
+		 * @see controller/jobs/product/export/filename
+		 * @see controller/jobs/product/export/domains
+		 */
+		return $this->context()->config()->get( 'controller/jobs/product/export/max-items', 10000 );
+	}
+
+
+	/**
+	 * Renders the output for the given items
+	 *
+	 * @param \Aimeos\Map $items List of product items implementing \Aimeos\MShop\Product\Item\Iface
+	 * @return string Rendered content
+	 */
+	protected function render( \Aimeos\Map $items ) : string
 	{
 		/** controller/jobs/product/export/template-items
 		 * Relative path to the XML items template of the product site map job controller.
@@ -188,8 +302,9 @@ class Standard
 		 *
 		 * @param string Relative path to the template creating XML code for the site map items
 		 * @since 2015.01
-		 * @see controller/jobs/product/export/template-footer
-		 * @see controller/jobs/product/export/template-index
+		 * @see controller/jobs/product/export/domains
+		 * @see controller/jobs/product/export/filename
+		 * @see controller/jobs/product/export/max-items
 		 */
 		$tplconf = 'controller/jobs/product/export/template-items';
 		$default = 'product/export/items-body-standard';
@@ -199,339 +314,6 @@ class Standard
 
 		$view->exportItems = $items;
 
-		$content->add( $view->render( $context->config()->get( $tplconf, $default ) ) );
-	}
-
-
-	/**
-	 * Creates a new container for the site map file
-	 *
-	 * @return \Aimeos\MW\Container\Iface Container object
-	 */
-	protected function createContainer() : \Aimeos\MW\Container\Iface
-	{
-		$config = $this->context()->config();
-
-		/** controller/jobs/product/export/location
-		 * Directory where the generated site maps should be placed into
-		 *
-		 * You have to configure a directory for the generated files on your
-		 * server that is writeable by the process generating the files, e.g.
-		 *
-		 * /var/www/your/export/path
-		 *
-		 * @param string Absolute directory to store the exported files into
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/container/options
-		 * @see controller/jobs/product/export/max-items
-		 * @see controller/jobs/product/export/max-query
-		 */
-		$location = $config->get( 'controller/jobs/product/export/location' );
-
-		/** controller/jobs/product/export/container/type
-		 * List of file container options for the export files
-		 *
-		 * The generated files are stored using container/content objects from
-		 * the core.
-		 *
-		 * @param string Container name
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/container/content
-		 * @see controller/jobs/product/export/container/options
-		 * @see controller/jobs/product/export/location
-		 * @see controller/jobs/product/export/max-items
-		 * @see controller/jobs/product/export/max-query
-		 */
-		$container = $config->get( 'controller/jobs/product/export/container/type', 'Directory' );
-
-		/** controller/jobs/product/export/container/content
-		 * List of file container options for the export files
-		 *
-		 * The generated files are stored using container/content objects from
-		 * the core.
-		 *
-		 * @param array Associative list of option name/value pairs
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/container/type
-		 * @see controller/jobs/product/export/container/options
-		 * @see controller/jobs/product/export/location
-		 * @see controller/jobs/product/export/max-items
-		 * @see controller/jobs/product/export/max-query
-		 */
-		$content = $config->get( 'controller/jobs/product/export/container/content', 'Binary' );
-
-		/** controller/jobs/product/export/container/options
-		 * List of file container options for the export files
-		 *
-		 * The generated files are stored using container/content objects from
-		 * the core.
-		 *
-		 * @param array Associative list of option name/value pairs
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/container/type
-		 * @see controller/jobs/product/export/container/content
-		 * @see controller/jobs/product/export/location
-		 * @see controller/jobs/product/export/max-items
-		 * @see controller/jobs/product/export/max-query
-		 */
-		$options = $config->get( 'controller/jobs/product/export/container/options', [] );
-
-		if( $location === null )
-		{
-			$msg = sprintf( 'Required configuration for "%1$s" is missing', 'controller/jobs/product/export/location' );
-			throw new \Aimeos\Controller\Jobs\Exception( $msg );
-		}
-
-		return \Aimeos\MW\Container\Factory::getContainer( $location, $container, $content, $options );
-	}
-
-
-	/**
-	 * Creates a new site map content object
-	 *
-	 * @param \Aimeos\MW\Container\Iface $container Container object
-	 * @param int $filenum New file number
-	 * @return \Aimeos\MW\Container\Content\Iface New content object
-	 */
-	protected function createContent( \Aimeos\MW\Container\Iface $container, int $filenum ) : \Aimeos\MW\Container\Content\Iface
-	{
-		/** controller/jobs/product/export/template-header
-		 * Relative path to the XML site map header template of the product site map job controller.
-		 *
-		 * The template file contains the XML code and processing instructions
-		 * to generate the site map header. The configuration string is the path
-		 * to the template file relative to the templates directory (usually in
-		 * templates/controller/jobs).
-		 *
-		 * You can overwrite the template file configuration in extensions and
-		 * provide alternative templates. These alternative templates should be
-		 * named like the default one but with the string "standard" replaced by
-		 * an unique name. You may use the name of your project for this. If
-		 * you've implemented an alternative client class as well, "standard"
-		 * should be replaced by the name of the new class.
-		 *
-		 * @param string Relative path to the template creating XML code for the site map header
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/template-items
-		 * @see controller/jobs/product/export/template-footer
-		 * @see controller/jobs/product/export/template-index
-		 */
-		$tplconf = 'controller/jobs/product/export/template-header';
-		$default = 'product/export/items-header-standard';
-
-		$context = $this->context();
-		$view = $context->view();
-
-		$content = $container->create( $this->getFilename( $filenum ) );
-		$content->add( $view->render( $context->config()->get( $tplconf, $default ) ) );
-		$container->add( $content );
-
-		return $content;
-	}
-
-
-	/**
-	 * Closes the site map content object
-	 *
-	 * @param \Aimeos\MW\Container\Content\Iface $content
-	 */
-	protected function closeContent( \Aimeos\MW\Container\Content\Iface $content )
-	{
-		/** controller/jobs/product/export/template-footer
-		 * Relative path to the XML site map footer template of the product site map job controller.
-		 *
-		 * The template file contains the XML code and processing instructions
-		 * to generate the site map footer. The configuration string is the path
-		 * to the template file relative to the templates directory (usually in
-		 * templates/controller/jobs).
-		 *
-		 * You can overwrite the template file configuration in extensions and
-		 * provide alternative templates. These alternative templates should be
-		 * named like the default one but with the string "standard" replaced by
-		 * an unique name. You may use the name of your project for this. If
-		 * you've implemented an alternative client class as well, "standard"
-		 * should be replaced by the name of the new class.
-		 *
-		 * @param string Relative path to the template creating XML code for the site map footer
-		 * @since 2015.01
-		 * @see controller/jobs/product/export/template-header
-		 * @see controller/jobs/product/export/template-items
-		 * @see controller/jobs/product/export/template-index
-		 */
-		$tplconf = 'controller/jobs/product/export/template-footer';
-		$default = 'product/export/items-footer-standard';
-
-		$context = $this->context();
-		$view = $context->view();
-
-		$content->add( $view->render( $context->config()->get( $tplconf, $default ) ) );
-	}
-
-
-	/**
-	 * Exports the products into the given container
-	 *
-	 * @param \Aimeos\MW\Container\Iface $container Container object
-	 * @param bool $default True to filter exported products by default criteria
-	 * @return array List of content (file) names
-	 */
-	protected function export( \Aimeos\MW\Container\Iface $container, bool $default = true ) : array
-	{
-		$domains = array( 'attribute', 'media', 'price', 'product', 'text' );
-
-		$domains = $this->getConfig( 'domains', $domains );
-		$maxItems = $this->getConfig( 'max-items', 10000 );
-		$maxQuery = $this->getConfig( 'max-query', 1000 );
-
-		$start = 0; $filenum = 1;
-		$names = [];
-
-		$manager = \Aimeos\MShop::create( $this->context(), 'product' );
-
-		$search = $manager->filter( $default );
-		$search->setSortations( array( $search->sort( '+', 'product.id' ) ) );
-		$search->slice( 0, $maxQuery );
-
-		$content = $this->createContent( $container, $filenum );
-		$names[] = $content->getResource();
-
-		do
-		{
-			$items = $manager->search( $search->slice( $start, $maxQuery ), $domains );
-			$remaining = $maxItems * $filenum - $start;
-			$count = count( $items );
-
-			if( $remaining < $count )
-			{
-				$this->addItems( $content, $items->slice( 0, $remaining ) );
-				$items = $items->slice( $remaining );
-
-				$this->closeContent( $content );
-				$content = $this->createContent( $container, ++$filenum );
-				$names[] = $content->getResource();
-			}
-
-			$this->addItems( $content, $items );
-			$start += $count;
-		}
-		while( $count >= $search->getLimit() );
-
-		$this->closeContent( $content );
-
-		return $names;
-	}
-
-
-	/**
-	 * Returns the configuration value for the given name
-	 *
-	 * @param string $name One of "domain", "max-items" or "max-query"
-	 * @param mixed $default Default value if name is unknown
-	 * @return mixed Configuration value
-	 */
-	protected function getConfig( string $name, $default = null )
-	{
-		$config = $this->context()->config();
-
-		switch( $name )
-		{
-			case 'domain':
-				/** controller/jobs/product/export/domains
-				 * List of associated items from other domains that should be exported too
-				 *
-				 * Products consist not only of the base data but also of texts, media
-				 * files, prices, attrbutes and other details. Those information is
-				 * associated to the products via their lists. Using the "domains" option
-				 * you can make more or less associated items available in the template.
-				 *
-				 * @param array List of domain names
-				 * @since 2015.01
-				 * @see controller/jobs/product/export/container/type
-				 * @see controller/jobs/product/export/container/content
-				 * @see controller/jobs/product/export/container/options
-				 * @see controller/jobs/product/export/filename
-				 * @see controller/jobs/product/export/location
-				 * @see controller/jobs/product/export/max-items
-				 * @see controller/jobs/product/export/max-query
-				 */
-				return $config->get( 'controller/jobs/product/export/domains', $default );
-
-			case 'max-items':
-				/** controller/jobs/product/export/max-items
-				 * Maximum number of exported products per file
-				 *
-				 * Limits the number of exported products per file as the memory
-				 * consumption of processing big files is rather high. Splitting
-				 * the data into several files that can also be processed in
-				 * parallel is able to speed up importing the files again.
-				 *
-				 * @param integer Number of products entries per file
-				 * @since 2015.01
-				 * @see controller/jobs/product/export/container/type
-				 * @see controller/jobs/product/export/container/content
-				 * @see controller/jobs/product/export/container/options
-				 * @see controller/jobs/product/export/filename
-				 * @see controller/jobs/product/export/location
-				 * @see controller/jobs/product/export/max-query
-				 * @see controller/jobs/product/export/domains
-				 */
-				return $config->get( 'controller/jobs/product/export/max-items', $default );
-
-			case 'max-query':
-				/** controller/jobs/product/export/max-query
-				 * Maximum number of products per query
-				 *
-				 * The products are fetched from the database in bunches for efficient
-				 * retrieval. The higher the value, the lower the total time the database
-				 * is busy finding the records. Higher values also means that record
-				 * updates in the tables need to wait longer and the memory consumption
-				 * of the PHP process is higher.
-				 *
-				 * @param integer Number of products per query
-				 * @since 2015.01
-				 * @see controller/jobs/product/export/container/type
-				 * @see controller/jobs/product/export/container/content
-				 * @see controller/jobs/product/export/container/options
-				 * @see controller/jobs/product/export/filename
-				 * @see controller/jobs/product/export/location
-				 * @see controller/jobs/product/export/max-items
-				 * @see controller/jobs/product/export/domains
-				 */
-				return $config->get( 'controller/jobs/product/export/max-query', $default );
-
-			case 'filename':
-				/** controller/jobs/product/export/filename
-				 * Template for the generated file names
-				 *
-				 * The generated export files will be named according to the given
-				 * string which can contain two place holders: The number of the
-				 * exported product and the ISO date/time when the file was created.
-				 *
-				 * @param string File name template
-				 * @since 2018.04
-				 * @see controller/jobs/product/export/container/type
-				 * @see controller/jobs/product/export/container/content
-				 * @see controller/jobs/product/export/container/options
-				 * @see controller/jobs/product/export/location
-				 * @see controller/jobs/product/export/max-items
-				 * @see controller/jobs/product/export/max-query
-				 * @see controller/jobs/product/export/domains
-				 */
-				return $config->get( 'controller/jobs/product/export/filename', $default );
-		}
-
-		return $default;
-	}
-
-
-	/**
-	 * Returns the file name for the new content file
-	 *
-	 * @param int $number Current file number
-	 * @return string New file name
-	 */
-	protected function getFilename( int $number ) : string
-	{
-		return sprintf( $this->getConfig( 'filename', 'aimeos-products-%1$d_%2$s.xml' ), $number, date( 'Y-m-d_H:i:s' ) );
+		return $view->render( $context->config()->get( $tplconf, $default ) );
 	}
 }
--- templates/controller/jobs/product/export/items-body-standard.php
+++ templates/controller/jobs/product/export/items-body-standard.php
@@ -8,31 +8,35 @@
 $enc = $this->encoder()
 
 ?>
-<?php foreach( $this->get( 'exportItems', [] ) as $item ) : ?>
-<productitem ref="<?= $enc->attr( $item->getCode() ) ?>">
-	<product.type><![CDATA[<?= $enc->xml( $item->getType() ) ?>]]></product.type>
-	<product.code><![CDATA[<?= $enc->xml( $item->getCode() ) ?>]]></product.code>
-	<product.label><![CDATA[<?= $enc->xml( $item->getLabel() ) ?>]]></product.label>
-	<product.status><![CDATA[<?= $enc->xml( $item->getStatus() ) ?>]]></product.status>
-	<product.config><![CDATA[<?= $enc->xml( json_encode( $item->getConfig() ) ) ?>]]></product.config>
-	<product.datestart><![CDATA[<?= $enc->xml( str_replace( ' ', 'T', $item->getDateStart() ?? '' ) ) ?>]]></product.datestart>
-	<product.dateend><![CDATA[<?= $enc->xml( str_replace( ' ', 'T', $item->getDateEnd() ?? '' ) ) ?>]]></product.dateend>
-	<lists>
-		<?php foreach( $item->getDomains() as $domain ) : ?>
-			<?= $this->partial(
-				'product/export/items-partial-' . str_replace( '/', '', $domain ) . '-standard',
-				['listItems' => $item->getListItems( $domain )]
-			) ?>
-		<?php endforeach ?>
-	</lists>
-	<property>
-		<?php foreach( $item->getPropertyItems() as $propItem ) : ?>
-			<propertyitem>
-				<product.property.type><![CDATA[<?= $enc->xml( $propItem->getType() ) ?>]]></product.property.type>
-				<product.property.languageid><![CDATA[<?= $enc->xml( $propItem->getLanguageId() ) ?>]]></product.property.languageid>
-				<product.property.value><![CDATA[<?= $enc->xml( $propItem->getValue() ) ?>]]></product.property.value>
-			</propertyitem>
-		<?php endforeach ?>
-	</property>
-</productitem>
-<?php endforeach ?>
+<?php echo '<?xml version="1.0" encoding="UTF-8"?>'; ?>
+<products>
+	<?php foreach( $this->get( 'exportItems', [] ) as $item ) : ?>
+		<productitem ref="<?= $enc->attr( $item->getCode() ) ?>">
+			<product.type><![CDATA[<?= $enc->xml( $item->getType() ) ?>]]></product.type>
+			<product.code><![CDATA[<?= $enc->xml( $item->getCode() ) ?>]]></product.code>
+			<product.label><![CDATA[<?= $enc->xml( $item->getLabel() ) ?>]]></product.label>
+			<product.boost><![CDATA[<?= $enc->xml( $item->boost() ) ?>]]></product.boost>
+			<product.status><![CDATA[<?= $enc->xml( $item->getStatus() ) ?>]]></product.status>
+			<product.config><![CDATA[<?= $enc->xml( json_encode( $item->getConfig() ) ) ?>]]></product.config>
+			<product.datestart><![CDATA[<?= $enc->xml( str_replace( ' ', 'T', $item->getDateStart() ?? '' ) ) ?>]]></product.datestart>
+			<product.dateend><![CDATA[<?= $enc->xml( str_replace( ' ', 'T', $item->getDateEnd() ?? '' ) ) ?>]]></product.dateend>
+			<lists>
+				<?php foreach( $item->getDomains() as $domain ) : ?>
+					<?= $this->partial(
+						'product/export/items-partial-' . str_replace( '/', '', $domain ) . '-standard',
+						['listItems' => $item->getListItems( $domain )]
+					) ?>
+				<?php endforeach ?>
+			</lists>
+			<property>
+				<?php foreach( $item->getPropertyItems() as $propItem ) : ?>
+					<propertyitem>
+						<product.property.type><![CDATA[<?= $enc->xml( $propItem->getType() ) ?>]]></product.property.type>
+						<product.property.languageid><![CDATA[<?= $enc->xml( $propItem->getLanguageId() ) ?>]]></product.property.languageid>
+						<product.property.value><![CDATA[<?= $enc->xml( $propItem->getValue() ) ?>]]></product.property.value>
+					</propertyitem>
+				<?php endforeach ?>
+			</property>
+		</productitem>
+	<?php endforeach ?>
+</products>
