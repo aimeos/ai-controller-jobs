743fa12312a696551afb1922ebfd8cf4f27e4474
Aimeos <aimeos@aimeos.org>
2024-02-26 11:09:29 +0100

    Fixed test

--- tests/Controller/Jobs/Product/Import/Csv/StandardTest.php
+++ tests/Controller/Jobs/Product/Import/Csv/StandardTest.php
@@ -229,8 +229,10 @@ class StandardTest extends \PHPUnit\Framework\TestCase
 	{
 		$stub = $this->getMockBuilder( '\\Aimeos\\MShop\\Product\\Manager\\Standard' )
 			->setConstructorArgs( [$this->context] )
-			->onlyMethods( ['delete'] )
+			->onlyMethods( ['save'] )
 			->getMock();
+
+		$stub->expects( $this->exactly( 1 ) )->method( 'save' );
 
 		\Aimeos\MShop::inject( '\\Aimeos\\MShop\\Product\\Manager\\Standard', $stub );
 
